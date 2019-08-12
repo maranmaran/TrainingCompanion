c5385b9cf4e22f81e9a5ce217eccc1d37694d5fe
Marko Urh <urh.marko@gmail.com>
2019-08-13 01:19:46 +0200

    test events

--- Frontend/src/app/shared/event-calendar/event-calendar.component.ts
+++ Frontend/src/app/shared/event-calendar/event-calendar.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import * as moment from 'moment'
 import { EventCalendar, CalendarEvent, CalendarDay } from './models/event-calendar.models';
 import { getEventCalendarModel, belongsToThisMonth, populateCalendar } from './models/event.calendar.utils';
+import { Observable } from 'rxjs';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-event-calendar',
@@ -14,31 +16,49 @@ export class EventCalendarComponent implements OnInit {
   currentDay = moment(new Date());
 
   @Output() selectDayEvent = new EventEmitter<CalendarDay>();
+  @Output() previousMonthEvent = new EventEmitter<number>();
+  @Output() currentMonthEvent = new EventEmitter<number>();
+  @Output() nextMonthEvent = new EventEmitter<number>();
 
-  @Input() events: CalendarEvent[];
+  @Input() events$: Observable<CalendarEvent[]>;
   @Input() eventIcon: string = '';
+
+  private subsink = new SubSink();
 
   constructor() { }
 
   ngOnInit() {
-    this.calendar = getEventCalendarModel(this.currentDay);     
-    this.calendar = populateCalendar(this.calendar, this.events);     
+    this.calendar = getEventCalendarModel(this.currentDay);
+    
+    this.subsink.add(
+      this.events$.subscribe(
+        events => {
+          console.log(events);
+          this.calendar = populateCalendar(this.calendar, events);
+        }
+      )
+    );
   }
-
 
   nextMonth() {
     this.currentDay = this.currentDay.clone().add(1, 'month');
     this.calendar = getEventCalendarModel(this.currentDay);
+
+    this.currentMonthEvent.emit(this.currentDay.month());
   }
 
   previousMonth() {
     this.currentDay = this.currentDay.clone().subtract(1, 'month');
     this.calendar = getEventCalendarModel(this.currentDay);
+
+    this.currentMonthEvent.emit(this.currentDay.month());
   }
 
   currentMonth() {
     this.currentDay = moment(new Date());
     this.calendar = getEventCalendarModel(this.currentDay);
+
+    this.currentMonthEvent.emit(this.currentDay.month());
   }
 
   selectDay(day: CalendarDay) {
