907e7c17758c879a2fb7f3290b146303172f693b
Marko Urh <urh.marko@gmail.com>
2019-08-13 01:19:46 +0200

    test events

--- Frontend/src/app/features/training-log/training-calendar/training-calendar.component.ts
+++ Frontend/src/app/features/training-log/training-calendar/training-calendar.component.ts
@@ -1,6 +1,8 @@
 import { CalendarEvent } from './../../../shared/event-calendar/models/event-calendar.models';
 import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment'
+import { Subject } from 'rxjs/internal/Subject';
+import { timeout } from 'q';
 
 @Component({
   selector: 'app-training-calendar',
@@ -9,15 +11,42 @@ import * as moment from 'moment'
 })
 export class TrainingCalendarComponent implements OnInit {
 
-  inputData: CalendarEvent[] = [
+  month1: CalendarEvent[] = [
+    {day: moment(new Date()).subtract(1, 'month'), event: 'event1'},
+    {day: moment(new Date()).subtract(1, 'month').add(1, 'day'), event: 'event2'},
+    {day: moment(new Date()).subtract(1, 'month').subtract(1, 'day'), event: 'event3'},
+  ] 
+  month2: CalendarEvent[] = [
     {day: moment(new Date()), event: 'event1'},
     {day: moment(new Date()).add(1, 'day'), event: 'event2'},
     {day: moment(new Date()).subtract(1, 'day'), event: 'event3'},
   ] 
+  month3: CalendarEvent[] = [
+    {day: moment(new Date()).add(1, 'month'), event: 'event1'},
+    {day: moment(new Date()).add(1, 'month').add(1, 'day'), event: 'event2'},
+    {day: moment(new Date()).add(1, 'month').subtract(1, 'day'), event: 'event3'},
+  ] 
+
+  inputData = new Subject<CalendarEvent[]>();
 
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.onMonthChange(moment(new Date()).month());
+    });
+  }
+
+  onMonthChange(month: number) {
+    if(month == 7) {
+      this.inputData.next(this.month2);
+    }
+    if(month > 7) {
+      this.inputData.next(this.month3);
+    }
+    if(month < 7) {
+      this.inputData.next(this.month1);
+    }
   }
 
 }
