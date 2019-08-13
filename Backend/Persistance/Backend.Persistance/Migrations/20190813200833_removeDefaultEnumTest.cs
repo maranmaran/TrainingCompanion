6a5b0f7cf1a6aaae7d8307f63028ed147c4d7555
Marko Urh <urh.marko@gmail.com>
2019-08-13 22:33:18 +0200

    no message

---
+++ Backend/Persistance/Backend.Persistance/Migrations/20190813200833_removeDefaultEnumTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Backend.Persistance.Migrations
+{
+    public partial class removeDefaultEnumTest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AccountType",
+                table: "Users",
+                nullable: false,
+                defaultValue: "Coach",
+                oldClrType: typeof(string),
+                oldDefaultValue: "Admin");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AccountType",
+                table: "Users",
+                nullable: false,
+                defaultValue: "Admin",
+                oldClrType: typeof(string),
+                oldDefaultValue: "Coach");
+        }
+    }
+}
