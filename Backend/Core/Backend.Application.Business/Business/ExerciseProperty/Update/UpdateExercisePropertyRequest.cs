b97f06be2d88bab80a20c02b009df27dbb3d8d70
Marko Urh <urh.marko@gmail.com>
2019-08-20 23:17:05 +0200

    crud - needs testing and UI

---
+++ Backend/Core/Backend.Application.Business/Business/ExerciseProperty/Update/UpdateExercisePropertyRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Backend.Application.Business.Business.ExerciseProperty.Create;
+using Backend.Application.Business.Business.ExerciseProperty.UpdateMany;
+using MediatR;
+
+namespace Backend.Application.Business.Business.ExerciseProperty.Update
+{
+
+    public class UpdateExercisePropertyRequest : IRequest<Domain.Entities.ExerciseType.ExerciseProperty>
+    {
+        public Guid Id { get; set; }
+        public string Value { get; set; }
+        public bool Active { get; set; }
+    }
+}
--- Backend/Core/Backend.Application.Business/Business/ExerciseProperty/UpdateMany/UpdateManyExercisePropertyRequestHandler.cs
+++ Backend/Core/Backend.Application.Business/Business/ExerciseProperty/UpdateMany/UpdateManyExercisePropertyRequestHandler.cs
@@ -9,38 +9,27 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Application.Business.Business.ExerciseProperty.UpdateMany
 {
-    public class UpdateExercisePropertyRequestHandler :
-        IRequestHandler<UpdateExercisePropertyRequest, IEnumerable<Domain.Entities.ExerciseType.ExerciseProperty>>
+    public class UpdateManyExercisePropertyRequestHandler : IRequestHandler<UpdateManyExercisePropertyRequest, Unit>
     {
         private readonly IApplicationDbContext _context;
 
-        public UpdateExercisePropertyRequestHandler(IApplicationDbContext context)
+        public UpdateManyExercisePropertyRequestHandler(IApplicationDbContext context)
         {
             _context = context;
         }
 
-        public async Task<IEnumerable<Domain.Entities.ExerciseType.ExerciseProperty>> Handle(UpdateExercisePropertyRequest request, CancellationToken cancellationToken)
+        public async Task<Unit> Handle(UpdateManyExercisePropertyRequest request, CancellationToken cancellationToken)
         {
             try
             {
-                var updatedProperties = new List<Domain.Entities.ExerciseType.ExerciseProperty>();
-
                 foreach (var requestExerciseProperty in request.ExerciseProperties)
                 {
-                    var property = await _context.ExerciseProperties.SingleAsync(x => x.Id == requestExerciseProperty.Id, cancellationToken);
-
-                    property.Value = requestExerciseProperty.Value;
-                    property.Order = requestExerciseProperty.Order;
-                    property.Active = requestExerciseProperty.Active;
-
-                    _context.ExerciseProperties.Update(property);
-
-                    updatedProperties.Add(property);
+                    _context.ExerciseProperties.Update(requestExerciseProperty);
                 }
 
                 await _context.SaveChangesAsync(cancellationToken);
 
-                return updatedProperties;
+                return Unit.Value;
             }
             catch (Exception e)
             {
