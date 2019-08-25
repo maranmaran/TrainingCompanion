40ab2760f9c1f8b1f8b23f33835af774a3e5a5af
Marko Urh <urh.marko@gmail.com>
2019-08-25 18:14:39 +0200

    module edit

--- Frontend/src/app/features/training-log/training-log-routing.module.ts
+++ Frontend/src/app/features/training-log/training-log-routing.module.ts
@@ -1,9 +1,10 @@
+import { TrainingsResolver } from './../../../business/resolvers/trainings.resolver';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TrainingLogHomeComponent } from './training-log-home/training-log-home.component';
 
 const routes: Routes = [
-    { path: '', component: TrainingLogHomeComponent, children: [
+    { path: '', resolve: {TrainingsResolver}, component: TrainingLogHomeComponent, children: [
     ]},
     { path: '**', redirectTo: '/' }, //always last
 ];
