ff7294a6600bad9b5b13faad9351eb6b8c08be88
Marko Urh <urh.marko@gmail.com>
2019-08-25 18:14:39 +0200

    module edit

--- Frontend/src/app/features/training-log/training-log.module.ts
+++ Frontend/src/app/features/training-log/training-log.module.ts
@@ -4,14 +4,20 @@ import { UIService } from 'src/business/services/shared/ui.service';
 import { TrainingLogRoutingModule } from './training-log-routing.module';
 import { TrainingLogHomeComponent } from './training-log-home/training-log-home.component';
 import { TrainingCalendarComponent } from './training-calendar/training-calendar.component';
+import { trainingReducer } from 'src/ngrx/training/training.reducers';
+import { TrainingEffects } from 'src/ngrx/training/training.effects';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { TrainingService } from 'src/business/services/training.service';
+import { TrainingsResolver } from 'src/business/resolvers/trainings.resolver';
 
 
 @NgModule({
     imports: [
         SharedModule,
         TrainingLogRoutingModule,
-        // StoreModule.forFeature('trainings', trainingsReducer),
-        // EffectsModule.forFeature([TrainingEffects])
+        StoreModule.forFeature('training', trainingReducer),
+        EffectsModule.forFeature([TrainingEffects]),
     ],
     declarations: [
         TrainingLogHomeComponent,
@@ -21,6 +27,8 @@ import { TrainingCalendarComponent } from './training-calendar/training-calendar
     ],
     providers: [
         UIService,
+        TrainingService,
+        TrainingsResolver
     ],
     entryComponents: [
     ]
