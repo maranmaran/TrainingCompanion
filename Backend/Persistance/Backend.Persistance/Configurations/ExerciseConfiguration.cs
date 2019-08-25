8e02f48528d06651658dfbe71f66a80067477991
Marko Urh <urh.marko@gmail.com>
2019-08-25 16:41:25 +0200

    Training log - entities

---
+++ Backend/Persistance/Backend.Persistance/Configurations/ExerciseConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Backend.Domain.Entities.TrainingLog;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Backend.Persistance.Configurations
+{
+    public class ExerciseConfiguration : IEntityTypeConfiguration<Exercise>
+    {
+        public void Configure(EntityTypeBuilder<Exercise> builder)
+        {
+
+
+        }
+    }
+}
