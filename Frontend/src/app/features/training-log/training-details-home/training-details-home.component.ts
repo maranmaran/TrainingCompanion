4a24ca08952bb848519c0c6789b7a52fe86d8d77
Marko Urh <urh.marko@gmail.com>
2019-08-26 22:22:21 +0200

    TAB GROUPS NEED SOME LOGIC WORK--- my brain can't compute now so...

---
+++ Frontend/src/app/features/training-log/training-details-home/training-details-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-training-details-home',
+  templateUrl: './training-details-home.component.html',
+  styleUrls: ['./training-details-home.component.css']
+})
+export class TrainingDetailsHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
