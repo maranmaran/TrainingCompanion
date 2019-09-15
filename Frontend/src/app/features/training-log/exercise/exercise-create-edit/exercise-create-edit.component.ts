f71bb95afff2957ba2f7a3a5fda5d6458855542f
Marko Urh <urh.marko@gmail.com>
2019-09-15 19:26:47 +0200

    fixed css glitch for height and tabs click

--- Frontend/src/app/features/training-log/exercise/exercise-create-edit/exercise-create-edit.component.ts
+++ Frontend/src/app/features/training-log/exercise/exercise-create-edit/exercise-create-edit.component.ts
@@ -13,7 +13,7 @@ import { selectedTraining } from 'src/ngrx/training-log/training/training.select
 import { CreateExerciseRequest } from 'src/server-models/cqrs/exercise/requests/create-exercise.request';
 import { ExerciseType } from 'src/server-models/entities/exercise-type.model';
 import { Exercise } from 'src/server-models/entities/exercise.model';
-import { ValidationErrors } from 'src/server-models/error/error-details.model';
+import { UpdateExerciseRequest } from 'src/server-models/cqrs/exercise/requests/update-exercise.request';
 
 @Component({
   selector: 'app-exercise-create-edit',
@@ -53,30 +53,11 @@ export class ExerciseCreateEditComponent implements OnInit {
 
 
   onSubmit() {
-    if (this.data.action == CRUD.Create) {
       this.createExercise();
-    } else {
-      this.updateExercise();
-    }
   }
 
   onClose(exercise?: Exercise) {
     this.dialogRef.close(exercise);
-  }
-
-  handleError(validationErrors: ValidationErrors) {
-    // if (validationErrors.status && validationErrors.status == ServerStatusCodes.ValidationError) {
-
-
-    //   if(validationErrors.errors.username) {
-    //     this.username.setErrors(validationErrors.errors.username)
-    //   }
-
-    //   if(validationErrors.errors.email) {
-    //     this.email.setErrors(validationErrors.errors.email)
-    //   }
-
-    // }
   }
 
   createExercise() {
@@ -96,61 +77,15 @@ export class ExerciseCreateEditComponent implements OnInit {
         return this.exerciseService.create<CreateExerciseRequest>(request)
       }
     ), take(1))
-    .subscribe(
-      (exercise: Exercise) => {
-        exercise.exerciseType = this.exerciseType.value;
-        this.store.dispatch(normalizeExercise({exercise}));
-        this.onClose(exercise);
-      },
-      err => console.log(err)
-    );
-    
+      .subscribe(
+        (exercise: Exercise) => {
+          exercise.exerciseType = this.exerciseType.value;
+          this.store.dispatch(normalizeExercise({ exercise, action: CRUD.Create }));
+          this.onClose(exercise);
+        },
+        err => console.log(err)
+      );
+
   }
-
-  updateExercise() {
-    this.exercise.exerciseTypeId = this.exerciseType.value.id;
-    this.exercise.exerciseType = this.exerciseType.value;
-
-    if (this.setsCount.value > this.exercise.sets.length) {// add
-      let sets = [];
-
-      for(var i = 0; i < (this.setsCount.value - this.exercise.sets.length); i++) {
-        sets.push(new Set());
-      };
-
-      this.exercise.sets = [...this.exercise.sets, ...sets];
-    } else if (this.setsCount.value < this.exercise.sets.length) { // delete 
-      var sets = [...this.exercise.sets];
-      for(var i = 0; i < this.setsCount.value; i++) {
-        sets.pop();;
-      };
-
-      this.exercise.sets = [...sets];
-    }
-
-    // this.updateTraining(false, this.exercise);
-  }
-
-  // updateTraining(newExercise: boolean, exercise: Exercise) {
-  //   this.store.select(selectedTraining).pipe(
-  //     take(1),
-  //     map(training => Object.assign({}, training)),
-  //     concatMap((training: Training) => {
-  //       var request = new UpdateTrainingRequest();
-  //       request.training = training;
-  //       request.exerciseAdd = exercise;
-  //       return this.trainingService.update<UpdateTrainingRequest, UpdateTrainingRequestResponse>(request);
-  //     }),
-  //     take(1)
-  //   ).subscribe((response: UpdateTrainingRequestResponse) => {
-
-  //     const trainingUpdate: Update<Training> = {
-  //       id: response.training.id,
-  //       changes: response.training
-  //     };
-
-  //     this.store.dispatch(trainingUpdated({ training: trainingUpdate }));
-  //     this.onClose(response.addedExercise);
-  //   });
-  // }
+ 
 }
--- Frontend/src/server-models/cqrs/exercise/requests/update-exercise.request.ts
+++ Frontend/src/server-models/cqrs/exercise/requests/update-exercise.request.ts
@@ -1,3 +1,7 @@
+import { Set } from './../../../entities/set.model';
 export class UpdateExerciseRequest {
-    
+    id: string;
+    exerciseTypeId: string;
+    trainingId: string;
+    sets: Set[];
 }
