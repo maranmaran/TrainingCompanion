0948dcc39b22b26c28a68ee5433f437e90d7885e
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2019-10-03 23:08:57 +0200

    Not implemented

--- Frontend/src/app/shared/shared.module.ts
+++ Frontend/src/app/shared/shared.module.ts
@@ -1,23 +1,24 @@
-import { MediaContainerComponent } from './media-container/media-container.component';
-import { ExerciseTypeChipListComponent } from './exercise-type-preview/exercise-type-chip-list/exercise-type-chip-list.component';
-import { ActiveFlagComponent } from './active-flag/active-flag.component';
-import { ExerciseTypePreviewComponent } from './exercise-type-preview/exercise-type-preview.component';
-import { EnumToArrayPipe } from './../../business/pipes/enum-to-array.pipe';
-import { ShowHidePasswordDirective } from 'src/business/directives/show-hide-password.directive';
-import { SanitizeHtmlPipe } from './../../business/pipes/sanitize-html.pipe';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { MaterialModule } from './angular-material.module';
-import { MediaDialogComponent } from './media-dialog/media-dialog.component';
-import { ErrorSnackbarComponent } from './error-snackbar/error-snackbar.component';
-import { MessageDialogComponent } from './message-dialog/message-dialog.component';
-import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
-import { MaterialTableComponent } from './material-table/material-table.component';
-import { EventCalendarComponent } from './event-calendar/event-calendar.component';
 import { DynamicModule } from 'ng-dynamic-component';
+import { ShowHidePasswordDirective } from 'src/business/directives/show-hide-password.directive';
+import { EnumToArrayPipe } from './../../business/pipes/enum-to-array.pipe';
+import { SanitizeHtmlPipe } from './../../business/pipes/sanitize-html.pipe';
+import { ActiveFlagComponent } from './active-flag/active-flag.component';
+import { MaterialModule } from './angular-material.module';
+import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
+import { ErrorSnackbarComponent } from './error-snackbar/error-snackbar.component';
+import { EventCalendarComponent } from './event-calendar/event-calendar.component';
+import { ExerciseTypeChipListComponent } from './exercise-type-preview/exercise-type-chip-list/exercise-type-chip-list.component';
 import { ExerciseTypeChipComponent } from './exercise-type-preview/exercise-type-chip/exercise-type-chip.component';
+import { ExerciseTypePreviewComponent } from './exercise-type-preview/exercise-type-preview.component';
+import { MaterialTableComponent } from './material-table/material-table.component';
+import { MediaContainerComponent } from './media-container/media-container.component';
+import { MediaDialogComponent } from './media-dialog/media-dialog.component';
+import { MessageDialogComponent } from './message-dialog/message-dialog.component';
+import { NotImplementedComponent } from './not-implemented/not-implemented.component';
 
 
 @NgModule({
@@ -26,7 +27,7 @@ import { ExerciseTypeChipComponent } from './exercise-type-preview/exercise-type
         ReactiveFormsModule,
         RouterModule,
         MaterialModule,
-        
+
         //https://www.npmjs.com/package/ng-dynamic-component
         DynamicModule.withComponents([ExerciseTypePreviewComponent, ExerciseTypeChipComponent, ExerciseTypeChipListComponent, ActiveFlagComponent])
     ],
@@ -44,7 +45,8 @@ import { ExerciseTypeChipComponent } from './exercise-type-preview/exercise-type
         ExerciseTypeChipComponent,
         ExerciseTypeChipListComponent,
         ActiveFlagComponent,
-        MediaContainerComponent
+        MediaContainerComponent,
+        NotImplementedComponent
     ],
     exports: [
         CommonModule,
@@ -65,7 +67,8 @@ import { ExerciseTypeChipComponent } from './exercise-type-preview/exercise-type
         ExerciseTypeChipComponent,
         ActiveFlagComponent,
         ExerciseTypeChipListComponent,
-        MediaContainerComponent
+        MediaContainerComponent,
+        NotImplementedComponent
     ],
     providers: [
     ],
