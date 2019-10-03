af9a5f6ddbaa17ca7ea1c782fc20200dd8001c94
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2019-10-03 23:08:57 +0200

    Not implemented

--- Frontend/src/app/features/training-log/training-log-routing.module.ts
+++ Frontend/src/app/features/training-log/training-log-routing.module.ts
@@ -1,8 +1,8 @@
-import { TrainingDetailsResolver } from './../../../business/resolvers/training-details.resolver';
-import { ExerciseDetailsResolver } from './../../../business/resolvers/exercise-details.resolver';
-import { ExerciseDetailsComponent } from './exercise/exercise-details/exercise-details.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotImplementedComponent } from 'src/app/shared/not-implemented/not-implemented.component';
+import { TrainingDetailsResolver } from './../../../business/resolvers/training-details.resolver';
+import { ExerciseDetailsComponent } from './exercise/exercise-details/exercise-details.component';
 import { TrainingLogHomeComponent } from './training-log-home/training-log-home.component';
 import { TrainingCalendarComponent } from './training/training-calendar/training-calendar.component';
 import { TrainingDetailsComponent } from './training/training-details/training-details.component';
@@ -12,8 +12,8 @@ const routes: Routes = [
         path: '', component: TrainingLogHomeComponent, children: [
             { path: '', redirectTo: 'calendar' },
             { path: 'calendar', component: TrainingCalendarComponent },
-            { path: 'week', component: TrainingCalendarComponent },
-            { path: 'list', component: TrainingCalendarComponent },
+            { path: 'week', component: NotImplementedComponent },
+            { path: 'list', component: NotImplementedComponent },
             { path: 'training-details', resolve: { TrainingDetailsResolver }, component: TrainingDetailsComponent },
             { path: 'exercise-details', resolve: { TrainingDetailsResolver }, component: ExerciseDetailsComponent }
         ]
