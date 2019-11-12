45d0e16ada4b41206341ff3272dcf33556c8b301
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2019-11-12 22:11:32 +0100

    no message

--- Frontend/src/app/features/dashboard/dashboard-home/dashboard-home.component.ts
+++ Frontend/src/app/features/dashboard/dashboard-home/dashboard-home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NotificationSignalrService } from 'src/business/services/feature-services/notification-signalr.service';
+import { NotificationType } from 'src/server-models/enums/notification-type.enum';
 
 @Component({
   selector: 'app-dashboard-home',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardHomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private notificationService: NotificationSignalrService
+  ) { }
 
   ngOnInit() {
+  }
+
+  activateNotif() {
+    this.notificationService.sendNotification(
+      NotificationType.TrainingCreated,
+      "Test notification text from client",
+      "E28EBA53-52E4-4FF9-AD02-FDA480BAFB67",
+      "91FBE82A-9BA3-4BEA-A744-8766751F8357");
   }
 
 }
