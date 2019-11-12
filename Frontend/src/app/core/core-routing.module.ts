f98c76c4cb64dc213e463c585060200860ee3c24
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2019-11-12 22:11:32 +0100

    no message

--- Frontend/src/app/core/core-routing.module.ts
+++ Frontend/src/app/core/core-routing.module.ts
@@ -1,14 +1,11 @@
-import { AppComponent } from './../app.component';
-import { AccountComponent } from './settings/account/account.component';
-import { BillingComponent } from './settings/billing/billing.component';
-import { GeneralComponent } from './settings/general/general.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from 'src/business/guards/auth.guard';
-import { AppContainerComponent } from './app-container/app-container.component';
-import { SubscriptionGuard } from 'src/business/guards/subscription.guard';
-import { IsCoach } from 'src/business/guards/is-coach.guard';
 import { CurrentUserLoadedGuard } from 'src/business/guards/current-user-loaded.guard';
+import { IsCoach } from 'src/business/guards/is-coach.guard';
+import { SubscriptionGuard } from 'src/business/guards/subscription.guard';
+import { AppContainerComponent } from './app-container/app-container.component';
+import { BillingComponent } from './settings/billing/billing.component';
 
 const routes: Routes = [
     { path: '', redirectTo: 'app', pathMatch: 'full' },
@@ -16,7 +13,7 @@ const routes: Routes = [
     { path: 'app', canActivate: [CurrentUserLoadedGuard], children: [
         {
             path: '', component: AppContainerComponent, canActivate: [AuthGuard, SubscriptionGuard],  children: [
-                { path: 'app', redirectTo: 'dashboard', pathMatch: 'full' },
+                { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
                 { path: 'dashboard', loadChildren: () => import('src/app/features/dashboard/dashboard.module').then(mod => mod.DashboardModule)},
                 { path: 'athletes', loadChildren: () => import('src/app/features/athlete-management/athletes.module').then(mod => mod.AthletesModule), canActivate: [IsCoach]},
                 { path: 'training-log', loadChildren: () => import('src/app/features/training-log/training-log.module').then(mod => mod.TrainingLogModule)},
