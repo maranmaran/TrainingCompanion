b2973baffddf217239f8eee9ff53821a59d10640
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2019-11-24 14:46:49 +0100

    Charts

--- Frontend/src/app/features/training-log/exercise/exercise-details/exercise-details.component.ts
+++ Frontend/src/app/features/training-log/exercise/exercise-details/exercise-details.component.ts
@@ -1,33 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { take } from 'rxjs/operators';
 import { AppState } from 'src/ngrx/global-setup.ngrx';
 import { selectedExercise } from 'src/ngrx/training-log/training/training.selectors';
 import { Exercise } from 'src/server-models/entities/exercise.model';
+import { SubSink } from 'subsink';
+import { UnitSystemService } from './../../../../../business/services/shared/unit-system.service';
 
 @Component({
   selector: 'app-exercise-details',
   templateUrl: './exercise-details.component.html',
   styleUrls: ['./exercise-details.component.scss']
 })
-export class ExerciseDetailsComponent implements OnInit {
+export class ExerciseDetailsComponent implements OnInit, OnDestroy {
 
   protected exercise: Exercise;
+  private subs = new SubSink();
 
   constructor(
-    private store: Store<AppState>
+    private store: Store<AppState>,
+    private unitService: UnitSystemService
   ) { }
 
   ngOnInit() {
-    this.store.select(selectedExercise).pipe(take(1))
-      .subscribe(exercise => this.exercise = exercise);
+    this.subs.add(
+      this.store.select(selectedExercise)
+        .subscribe(exercise => this.exercise = exercise)
+      );
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
   }
 
   get totalVolume() {
-    return this.exercise.sets.reduce((total, set) => total + set.volume, 0);
+    const volume = this.exercise.sets.reduce((total, set) => total + set.volume, 0);
+    return this.unitService.transformWeight(volume);
   }
 
   get projectedMax() {
-    return this.exercise.sets.reduce((max, set) => set.projectedMax > max ? set.projectedMax : max, 0);
+    const max = this.exercise.sets.reduce((max, set) => set.projectedMax > max ? set.projectedMax : max, 0);
+    return this.unitService.transformWeight(max);
   }
 }
