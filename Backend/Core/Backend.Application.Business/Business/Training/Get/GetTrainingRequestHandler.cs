8c60ee2cb203f1f41cbe8270220dcaa7d06392ab
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2019-12-14 22:38:22 +0100

    Check s3

--- Backend/Core/Backend.Application.Business/Business/Training/Get/GetTrainingRequestHandler.cs
+++ Backend/Core/Backend.Application.Business/Business/Training/Get/GetTrainingRequestHandler.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Backend.Domain;
+using Backend.Service.AmazonS3.Interfaces;
 using Backend.Service.Infrastructure.Exceptions;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -12,13 +13,15 @@ namespace Backend.Application.Business.Business.Training.Get
     public class GetTrainingRequestHandler : IRequestHandler<GetTrainingRequest, Domain.Entities.TrainingLog.Training>
     {
         private readonly IApplicationDbContext _context;
+        private readonly IS3AccessService _s3AccessService;
 
-        public GetTrainingRequestHandler(IApplicationDbContext context)
+        public GetTrainingRequestHandler(IApplicationDbContext context, IS3AccessService s3AccessService)
         {
             _context = context;
+            _s3AccessService = s3AccessService;
         }
 
-        public Task<Domain.Entities.TrainingLog.Training> Handle(GetTrainingRequest request, CancellationToken cancellationToken)
+        public async Task<Domain.Entities.TrainingLog.Training> Handle(GetTrainingRequest request, CancellationToken cancellationToken)
         {
             try
             {
@@ -40,11 +43,35 @@ namespace Backend.Application.Business.Business.Training.Get
 
                     .FirstOrDefault(x => x.Id == request.TrainingId);
 
-                return Task.FromResult(training);
+                await RefreshPresignedUrls(training);
+
+                return training;
             }
             catch (Exception e)
             {
                 throw new NotFoundException(nameof(Domain.Entities.TrainingLog.Training), $"Could not find training for {request.TrainingId} Training", e);
+            }
+        }
+
+
+        /// <summary>
+        /// Refresh all pre-signed urls that need to be refreshed
+        /// </summary>
+        /// <param name="training"></param>
+        /// <returns></returns>
+        private async Task RefreshPresignedUrls(Domain.Entities.TrainingLog.Training training)
+        {
+            foreach (var media in training.Media)
+            {
+                media.DownloadUrl = await _s3AccessService.RenewPresignedUrl(media.DownloadUrl, media.Filename);
+            }
+
+            foreach (var exercise in training.Exercises)
+            {
+                foreach (var media in exercise.Media)
+                {
+                    media.DownloadUrl = await _s3AccessService.RenewPresignedUrl(media.DownloadUrl, media.Filename);
+                }
             }
         }
     }
