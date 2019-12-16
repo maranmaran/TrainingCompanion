47d7202bace56b15e2854c073f3e5a41fbd3cf62
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2019-12-16 23:23:48 +0100

    no message

---
+++ Backend/Core/Backend.Application.Business/Business/PushNotification/ReadNotification/ReadNotificationRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+
+namespace Backend.Application.Business.Business.PushNotification.ReadNotification
+{
+    public class ReadNotificationRequest: IRequest<Unit>
+    {
+        public Guid Id { get; set; }      
+    }
+}
---
+++ Frontend/src/app/features/training-log/training/training-details/training-details-data/training-details-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-training-details-data',
+  templateUrl: './training-details-data.component.html',
+  styleUrls: ['./training-details-data.component.scss']
+})
+export class TrainingDetailsDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
