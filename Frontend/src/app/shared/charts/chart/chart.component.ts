d0b8994509ec7a0097116c72b1063791f5a93915
Marko Urh <urh.marko@gmail.com>
2019-12-18 00:10:30 +0100

    no message

--- Frontend/src/app/shared/charts/chart/chart.component.ts
+++ Frontend/src/app/shared/charts/chart/chart.component.ts
@@ -3,26 +3,27 @@ import * as Chart from 'chart.js';
 import { ChartConfiguration } from 'chart.js';
 
 @Component({
-  selector: 'app-line-chart',
-  templateUrl: './line-chart.component.html',
-  styleUrls: ['./line-chart.component.scss']
+  selector: 'app-chart',
+  templateUrl: './chart.component.html',
+  styleUrls: ['./chart.component.scss']
 })
-export class LineChartComponent implements OnInit {
+export class ChartComponent implements OnInit {
 
   chart: Chart;
   canvasCtx: CanvasRenderingContext2D;
 
   @Input() configuration: ChartConfiguration;
-  @ViewChild('lineChartCanvas', {static: true}) canvas2: ElementRef;
+  @ViewChild('chartCanvas', { static: true }) canvas: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
 
-    const ctx = (<HTMLCanvasElement>this.canvas2.nativeElement).getContext('2d');
+    const ctx = (<HTMLCanvasElement>this.canvas.nativeElement).getContext('2d');
 
     this.chart = new Chart(ctx, this.configuration);
 
   }
+
 
 }
