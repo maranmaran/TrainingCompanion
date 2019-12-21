ffbc31b5468bb82f1e2dbc40cc827e14c699ff07
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2019-12-21 01:58:11 +0100

    Pretty export

---
+++ Backend/Core/Services/Backend.Service.Excel/Models/IExportDataContainer.cs
@@ -0,0 +1,11 @@
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Style.XmlAccess;
+
+namespace Backend.Service.Excel.Models
+{
+    public interface IExportDataContainer
+    {
+        (string Title, string Author, string Comments, string Company) GetExportFileProperties();
+        void WriteToSheet(ExcelWorksheet sheet, (ExcelNamedStyleXml HeaderStyle, ExcelNamedStyleXml CellStyle) styles);
+    }
+}
