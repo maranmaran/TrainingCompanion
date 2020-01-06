92496620c69c303cae2f6b1088f5cf43c5d9912e
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-01-06 16:51:09 +0100

    no message

--- Frontend/src/app/features/export-import/export-import-home/import/exercise-type-import/exercise-type-import.component.ts
+++ Frontend/src/app/features/export-import/export-import-home/import/exercise-type-import/exercise-type-import.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { take } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { UIProgressBar } from 'src/business/shared/ui-progress-bars.enum';
+import { AppState } from 'src/ngrx/app/app.state';
+import { setActiveProgressBar } from 'src/ngrx/user-interface/ui.actions';
 import { ImportExerciseTypeRequest } from 'src/server-models/cqrs/import/request/import.request';
 import { ImportService } from './../../../../../../business/services/feature-services/import.service';
-import { currentUserId } from './../../../../../../ngrx/auth/auth.selectors';
-import { AppState } from './../../../../../../ngrx/global-setup.ngrx';
 import { ImportResponse } from './../../../../../../server-models/cqrs/import/response/import.response';
 
 @Component({
@@ -14,23 +15,38 @@ import { ImportResponse } from './../../../../../../server-models/cqrs/import/re
 })
 export class ExerciseTypeImportComponent implements OnInit {
 
-  _userId: string;
-
   constructor(
     private store: Store<AppState>,
     private importService: ImportService
   ) { }
 
   ngOnInit() {
-    this.store.select(currentUserId).pipe(take(1)).subscribe(id => this._userId = id);
   }
 
-  import(file: File) {
-    var request = new ImportExerciseTypeRequest(this._userId, file);
+
+  public get isUploadingObs() : Observable<{uploading: boolean, response: ImportResponse}> {
+    return this._isUploading.asObservable()
+  }
+
+  _isUploading = new BehaviorSubject<{uploading: boolean, response: ImportResponse}>({uploading: false, response: null});
+
+  import = (file: File, userId: string) => {
+
+    this.store.dispatch(setActiveProgressBar({ progressBar: UIProgressBar.None }))
+    this._isUploading.next({uploading: true, response: null});
+
+    var request = new ImportExerciseTypeRequest(userId, file);
     this.importService.importExerciseType(request)
       .subscribe(
-        (response: ImportResponse) => console.log(response),
-        err => console.log(err)
+        (response: ImportResponse) => {
+          console.log(response);
+          this._isUploading.next({uploading: false, response})
+        },
+        err => {
+          console.log(err);
+          this._isUploading.next({uploading: false, response: null})
+        },
+        () => this.store.dispatch(setActiveProgressBar({ progressBar: UIProgressBar.MainAppScreen }))
       );
   }
 
--- Frontend/src/app/features/export-import/export-import-home/import/import-button/import-button.component.ts
+++ Frontend/src/app/features/export-import/export-import-home/import/import-button/import-button.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { AppState } from 'src/ngrx/app/app.state';
+import { currentUserId } from 'src/ngrx/auth/auth.selectors';
+import { ImportResponse } from 'src/server-models/cqrs/import/response/import.response';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-import-button',
@@ -8,14 +15,22 @@ import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild }
 export class ImportButtonComponent implements OnInit {
 
   @Input() fileTypesToAccept: string = '.csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel';
-  @Output() fileUploaded = new EventEmitter<File>();
+  @Input() importFn: (file: File, userId: string) => void;
+  @Input() isUploading: Observable<{uploading: boolean, response: ImportResponse}>;
 
   @ViewChild('uploadInput', {static: false}) uploadInput: ElementRef;
 
+  _userId: string;
+
+  _subs = new SubSink();
+
   constructor(
+    private store: Store<AppState>,
   ) { }
 
-  ngOnInit() {  }
+  ngOnInit() {
+    this.store.select(currentUserId).pipe(take(1)).subscribe(id => this._userId = id);
+  }
 
   triggerUploadMediaInput() {
     this.uploadInput.nativeElement.click();
@@ -23,8 +38,12 @@ export class ImportButtonComponent implements OnInit {
 
   uploadMedia(files: FileList) {
     if(files.item(0)) {
-      this.fileUploaded.emit(files.item(0));
+      this.onImport(files.item(0));
       this.uploadInput.nativeElement.value = '';
     }
+  }
+
+  onImport(file: File) {
+    this.importFn(file, this._userId);
   }
 }
