28afb8340a5c3b88e5c34db053b248edaec3e840
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-01-07 22:18:38 +0100

    no message

--- Frontend/src/app/features/training-log/training/training-details/panels/training-metrics/training-metrics.component.ts
+++ Frontend/src/app/features/training-log/training/training-details/panels/training-metrics/training-metrics.component.ts
@@ -1,26 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { getTestBarChartConfig, getTestLineChartConfig, getTestPieChartConfig, getTestPolarAreaChart, getTestHorizontalChartConfig, getTestHorizontalStackedChartConfig } from 'src/app/shared/charts/chart-config.factory';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { ChartConfiguration } from 'chart.js';
+import { getBarChartConfig, getHorizontalChartConfig, getHorizontalStackedChartConfig, getPieChartConfig, getPolarAreaChart } from 'src/app/shared/charts/training-chart-config.factory';
+import { AppState } from 'src/ngrx/app/app.state';
+import { SubSink } from 'subsink';
+import { Theme } from './../../../../../../../business/shared/theme.enum';
+import { activeTheme } from './../../../../../../../ngrx/user-interface/ui.selectors';
 
 @Component({
   selector: 'app-training-metrics',
   templateUrl: './training-metrics.component.html',
   styleUrls: ['./training-metrics.component.scss']
 })
-export class TrainingMetricsComponent implements OnInit {
+export class TrainingMetricsComponent implements OnInit, OnDestroy {
 
-  totalVolumeChart = getTestBarChartConfig(); // 1
-  volumeSplitChart = getTestPieChartConfig(); // 2
+  totalVolumeChart: ChartConfiguration; // 1
+  volumeSplitChart: ChartConfiguration // 2
 
-  numberOfLiftsChart = getTestHorizontalChartConfig(); // 3
-  weightedAverageIntensityChart = getTestHorizontalStackedChartConfig(); // 4
+  numberOfLiftsChart: ChartConfiguration; // 3
+  weightedAverageIntensityChart: ChartConfiguration; // 4
 
   // max heavy medium light deload
   // total and per exercise
-  zoneOfIntensity = getTestPolarAreaChart(); // 5
+  zoneOfIntensity: ChartConfiguration; // 5
 
-  constructor() { }
+  _subs = new SubSink();
+  _theme: Theme;
+
+  constructor(
+    private store: Store<AppState>
+  ) { }
 
   ngOnInit() {
+    this._subs.add(
+      this.store.select(activeTheme).subscribe(theme => {
+        this._theme = theme;
+        this.getChartConfigs();
+      })
+    )
+
+  }
+
+  data = [1,2,3]
+  labels = ['Exercise 1', 'Exercise 2', 'Exercise 3']
+  getChartConfigs() {
+    this.volumeSplitChart = getPieChartConfig(this._theme, this.data, this.labels);
+    this.totalVolumeChart = getBarChartConfig(this._theme, this.data, this.labels);
+    this.numberOfLiftsChart = getHorizontalChartConfig(this._theme, this.data, this.labels);
+    this.weightedAverageIntensityChart = getHorizontalStackedChartConfig(this._theme, this.data, this.labels);
+    this.zoneOfIntensity = getPolarAreaChart(this._theme, this.data, this.labels);
+  }
+
+  ngOnDestroy(): void {
+    this._subs.unsubscribe();
   }
 
 }
---
+++ Frontend/src/app/shared/charts/training-chart-config.factory.ts
@@ -0,0 +1,271 @@
+import * as Chart from 'chart.js';
+import { ChartConfiguration } from 'chart.js';
+import { Guid } from 'guid-typescript';
+import { Theme } from './../../../business/shared/theme.enum';
+
+export const chartColorsLightTheme = {
+  red: 'rgb(255, 99, 132)',
+  orange: 'rgb(255, 159, 64)',
+  yellow: 'rgb(255, 205, 86)',
+  green: 'rgb(75, 192, 192)',
+  blue: 'rgb(54, 162, 235)',
+  purple: 'rgb(153, 102, 255)',
+  grey: 'rgb(231,233,237)'
+};
+
+export const chartColorsDarkTheme = {
+  blue: 'rgb(54, 162, 235)',
+  purple: 'rgb(153, 102, 255)',
+  red: 'rgb(255, 99, 132)',
+  orange: 'rgb(255, 159, 64)',
+  yellow: 'rgb(255, 205, 86)',
+  green: 'rgb(75, 192, 192)',
+  grey: 'rgb(231,233,237)'
+};
+
+export const getChartColors = (theme: Theme) => theme == Theme.Dark ? chartColorsDarkTheme : chartColorsLightTheme;
+
+export const colorsArr = (theme: Theme): string[] => Object.values(getChartColors(theme));
+export const backgroundColors = (from, to, theme) => colorsArr(theme).slice(from, to);
+export const fontColor = (theme: Theme) => theme == Theme.Dark ? 'white' : 'black';
+export const colorHelpers = Chart.helpers.color;
+
+export interface MyChartConfiguration extends ChartConfiguration {
+  generationId: Guid
+}
+
+export function getPieChartConfig(theme: Theme, data: number[], labels: string[]): MyChartConfiguration {
+
+  return {
+    generationId: Guid.create(),
+    type: 'pie',
+    data: {
+      datasets: [{
+        data,
+        backgroundColor: backgroundColors(0, data.length, theme),
+      }],
+      labels
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      legend: {
+        position: 'top',
+        labels: {
+          fontColor: fontColor(theme)
+        }
+      },
+    }
+  };
+}
+
+export function getBarChartConfig(theme: Theme, data: number[], labels: string[]): MyChartConfiguration {
+  return {
+    generationId: Guid.create(),
+    type: 'bar',
+    data: {
+      datasets: [
+        {
+          data,
+          barThickness: 10,
+          maxBarThickness: 20,
+          backgroundColor: backgroundColors(0, data.length, theme),
+        }
+      ],
+      labels
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      legend: {
+        display: false
+      },
+      scales: {
+        xAxes: [{
+          ticks: {
+            fontColor: fontColor(theme)
+          },
+          gridLines: {
+            color: colorHelpers(fontColor(theme)).alpha(0.15).rgbString()
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+            fontColor: fontColor(theme)
+          },
+          gridLines: {
+            color: colorHelpers(fontColor(theme)).alpha(0.15).rgbString()
+          }
+        }]
+      }
+    }
+  };
+}
+
+export function getLineChartConfig(theme: Theme, data: number[], labels: string[]): MyChartConfiguration {
+  return {
+    generationId: Guid.create(),
+    type: 'line',
+    data: {
+      datasets: [
+        {
+          data,
+        }
+      ],
+      labels
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      legend: {
+        position: 'top',
+        labels: {
+          fontColor: fontColor(theme)
+        }
+      },
+      scales: {
+        xAxes: [{
+          ticks: {
+            fontColor: fontColor(theme)
+          },
+          gridLines: {
+            color: colorHelpers(fontColor(theme)).alpha(0.15).rgbString()
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            fontColor: fontColor(theme)
+          },
+          gridLines: {
+            color: colorHelpers(fontColor(theme)).alpha(0.15).rgbString()
+          }
+        }]
+      }
+    }
+  };
+}
+
+export function getHorizontalChartConfig(theme: Theme, data: number[], labels: string[]): MyChartConfiguration {
+  return {
+    generationId: Guid.create(),
+    type: 'horizontalBar',
+    data: {
+      datasets: [{
+        data,
+        backgroundColor: backgroundColors(0, data.length, theme),
+        barThickness: 10,
+        maxBarThickness: 20,
+      }],
+      labels
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      legend: {
+        display: false
+      },
+      scales: {
+        xAxes: [{
+          ticks: {
+            beginAtZero: true,
+            fontColor: fontColor(theme)
+          },
+          gridLines: {
+            color: colorHelpers(fontColor(theme)).alpha(0.15).rgbString()
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            fontColor: fontColor(theme)
+          },
+          gridLines: {
+            color: colorHelpers(fontColor(theme)).alpha(0.15).rgbString()
+          }
+        }]
+      }
+    }
+  };
+}
+
+export function getHorizontalStackedChartConfig(theme: Theme, data: number[], labels: string[]): MyChartConfiguration {
+  return {
+    generationId: Guid.create(),
+    type: 'horizontalBar',
+    data: {
+      datasets: [
+        {
+          data,
+          backgroundColor: backgroundColors(0, data.length, theme),
+          barThickness: 10,
+          maxBarThickness: 20
+        },
+        {
+          data,
+          backgroundColor: backgroundColors(0, data.length, theme),
+          barThickness: 10,
+          maxBarThickness: 20,
+        }
+      ],
+      labels
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      legend: {
+        display: false
+      },
+      scales: {
+        xAxes: [{
+          stacked: true,
+          ticks: {
+            fontColor: fontColor(theme)
+          },
+          gridLines: {
+            color: colorHelpers(fontColor(theme)).alpha(0.15).rgbString()
+          }
+        }],
+        yAxes: [{
+          stacked: true,
+          ticks: {
+            fontColor: fontColor(theme)
+          },
+          gridLines: {
+            color: colorHelpers(fontColor(theme)).alpha(0.15).rgbString()
+          }
+        }]
+      },
+    }
+  };
+}
+
+export function getPolarAreaChart(theme: Theme, data: number[], labels: string[]): MyChartConfiguration {
+  return {
+    generationId: Guid.create(),
+    type: 'polarArea',
+    data: {
+      datasets: [{
+        data,
+        backgroundColor: backgroundColors(0, data.length, theme),
+      }],
+      labels
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      legend: {
+        position: 'top',
+        labels: {
+          fontColor: fontColor(theme)
+        }
+      },
+      scale: {
+        ticks: {
+          display: false
+        }
+      }
+    }
+  };
+}
+
+
