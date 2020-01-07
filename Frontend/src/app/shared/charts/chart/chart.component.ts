fcfcf51d5bb612cab0745be8f7c433c0a9391934
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-01-07 22:18:38 +0100

    no message

--- Frontend/src/app/shared/charts/chart/chart.component.ts
+++ Frontend/src/app/shared/charts/chart/chart.component.ts
@@ -1,18 +1,18 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import * as Chart from 'chart.js';
-import { ChartConfiguration } from 'chart.js';
+import { MyChartConfiguration } from '../training-chart-config.factory';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.scss']
 })
-export class ChartComponent implements OnInit {
+export class ChartComponent implements OnInit, OnChanges {
 
   chart: Chart;
   canvasCtx: CanvasRenderingContext2D;
 
-  @Input() configuration: ChartConfiguration;
+  @Input() configuration: MyChartConfiguration;
   @ViewChild('chartCanvas', { static: true }) canvas: ElementRef;
 
   constructor() { }
@@ -23,6 +23,22 @@ export class ChartComponent implements OnInit {
 
     this.chart = new Chart(ctx, this.configuration);
 
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+
+    var cur = changes.configuration.currentValue;
+    var prev = changes.configuration.previousValue;
+
+    if(cur && prev && cur.generationId.value != prev.generationId.value)
+      this.updateChart();
+
+  }
+
+  updateChart() {
+    this.chart.options = this.configuration.options;
+    this.chart.data = this.configuration.data;
+    this.chart.update();
   }
 
 
