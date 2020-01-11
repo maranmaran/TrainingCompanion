d5c1e2d76254b6411357704cc78730ac6a0fd869
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-01-11 12:55:46 +0100

    no message

---
+++ Backend/Core/Backend.Application.Business/Business/Import/GetImportSample/GetImportSampleRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Application.Business.Business.Import.GetImportSample
+{
+    public class GetImportSampleRequest : IRequest<FileContentResult>
+    {
+        public ImportType ImportType { get; set; }
+        public SampleType SampleType { get; set; }
+    }
+}
--- Backend/Core/Services/Backend.Service.Excel/Interfaces/IExcelService.cs
+++ Backend/Core/Services/Backend.Service.Excel/Interfaces/IExcelService.cs
@@ -1,7 +1,7 @@
 ﻿using Backend.Common;
-using Backend.Service.Excel.Models.Export;
 using Backend.Service.Excel.Models.Import;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,7 +10,7 @@ namespace Backend.Service.Excel.Interfaces
 {
     public interface IExcelService
     {
-        Task<ExportResult> Export(IExportDataContainer data, CancellationToken cancellationToken);
+        Task<FileContentResult> Export(IExportDataContainer data, CancellationToken cancellationToken);
 
         Task<IEnumerable<T>> ParseImportData<T>(IFormFile file, CancellationToken cancellationToken) where T : new();
 
