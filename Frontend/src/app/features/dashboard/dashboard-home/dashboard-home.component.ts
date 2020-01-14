cdd96e12618ea785832dc431da62d5ae7b766dca
Marko Urh <urh.marko@gmail.com>
2020-01-14 13:54:30 +0100

    Dynamic dashboard

--- Frontend/src/app/features/dashboard/dashboard-home/dashboard-home.component.ts
+++ Frontend/src/app/features/dashboard/dashboard-home/dashboard-home.component.ts
@@ -1,27 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList, ChangeDetectorRef, ComponentFactoryResolver, AfterViewInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { take } from 'rxjs/operators';
 import { NotificationSignalrService } from 'src/business/services/feature-services/notification-signalr.service';
 import { AppState } from 'src/ngrx/app/app.state';
 import { NotificationType } from 'src/server-models/enums/notification-type.enum';
 import { currentUserId } from './../../../../ngrx/auth/auth.selectors';
+import { DashboardOutletDirective } from '../directives/dashboard-outlet.directive';
+import { DashboardItem } from '../models/dashboard-item.interface';
+import { dashboardCards, DashboardCards } from '../models/dashboard-cards';
+import { DashboardCardContainerComponent } from '../dashboard-card-container/dashboard-card-container.component';
+import { Track } from '../models/track.interface';
+import { DashboardService } from '../services/dashboard.service';
 
 @Component({
   selector: 'app-dashboard-home',
   templateUrl: './dashboard-home.component.html',
   styleUrls: ['./dashboard-home.component.scss']
 })
-export class DashboardHomeComponent implements OnInit {
+export class DashboardHomeComponent implements OnInit, AfterViewInit {
 
+  @ViewChildren(DashboardOutletDirective) dashboardOutlets: QueryList<DashboardOutletDirective>;
   private userId: string;
+  tracks: Track[];
+ 
 
   constructor(
     private notificationService: NotificationSignalrService,
-    private store: Store<AppState>
+    private store: Store<AppState>,
+    private cd: ChangeDetectorRef,
+    private cfr: ComponentFactoryResolver,
+    private dashboardService: DashboardService
   ) { }
 
   ngOnInit() {
     this.store.select(currentUserId).pipe(take(1)).subscribe(userId => this.userId = userId);
+
+    this.dashboardService.tracks$.subscribe(tracks => {
+      this.tracks = tracks;
+      this.cd.detectChanges();
+      this.loadContents();
+    });
+  }
+
+  ngAfterViewInit() {
+    this.loadContents();
   }
 
   activateNotif() {
@@ -31,5 +53,32 @@ export class DashboardHomeComponent implements OnInit {
       this.userId,
       this.userId);
   }
+
+  loadContents = () => {
+    if(!this.dashboardOutlets || !this.dashboardOutlets.length) 
+      return;
+
+      this.dashboardOutlets.forEach(template => {
+        this.cd.detectChanges();
+        this.loadContent(template, template.item);
+      });
+
+      this.cd.detectChanges();
+  }
+
+  loadContent = (template: DashboardOutletDirective, item: DashboardItem) => {
+    if (!item.component)
+      return;
+
+    const viewContainerRef = template.viewContainerRef;
+    viewContainerRef.clear();
+
+    const factory = this.cfr.resolveComponentFactory(dashboardCards[item.component]);
+    const componentRef = viewContainerRef.createComponent(factory);
+    const instance = componentRef.instance as DashboardCardContainerComponent;
+    instance.item = item;
+  }
+
+
 
 }
---
+++ Frontend/src/app/features/dashboard/services/dashboard.service.ts
@@ -0,0 +1,106 @@
+import { Injectable, OnInit } from "@angular/core";
+import { Track } from '../models/track.interface';
+import { DashboardCards } from '../models/dashboard-cards';
+import { BehaviorSubject } from 'rxjs';
+import { BaseService } from 'src/business/services/base.service';
+import { HttpClient } from '@angular/common/http';
+import { catchError, take } from 'rxjs/operators';
+import { AppState } from 'src/ngrx/app/app.state';
+import { Store } from '@ngrx/store';
+import { currentUserId } from 'src/ngrx/auth/auth.selectors';
+import { DashboardItem } from '../models/dashboard-item.interface';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DashboardService extends BaseService implements OnInit {
+
+    constructor(
+        private httpDI: HttpClient,
+        private store: Store<AppState>
+    ) {
+        super(httpDI, 'Dashboard');
+    }
+
+    // two tracks supported
+    private _defaultState: Track[] = [
+        {
+            items: [
+                {
+                    component: DashboardCards.Test,
+                    id: 'test'
+                }
+            ]
+        },
+        {
+            items: [
+                {
+                    component: DashboardCards.Test,
+                    id: 'test'
+                }
+            ]
+        }
+    ]
+
+    private _userId: string;
+    private _trackState = new BehaviorSubject<Track[]>(this._defaultState);
+    tracks$ = this._trackState.asObservable();
+
+    ngOnInit() {
+        this.store.select(currentUserId).pipe(take(1)).subscribe(id => this._userId = id);
+    }
+
+    public getUserTracks() {
+        this.http
+            .get<Track[]>(this.url + 'GetUserTracks/' + this._userId)
+            .pipe(
+                catchError(this.handleError)
+            ).subscribe(
+                (tracks: Track[]) => this._trackState.next(tracks),
+                err => console.log(err)
+            );
+    }
+
+    public saveUserTracks(tracks: Track[]) {
+        var request = { userId: this._userId, tracks: tracks };
+
+        this.http
+            .post(this.url + 'SaveUserTracks/', request)
+            .pipe(
+                catchError(this.handleError)
+            ).subscribe(
+                () => {},
+                err => console.log(err)
+            );
+    }
+
+    public removeItem = (item: DashboardItem) => {
+        const state = this._trackState.getValue();
+        
+        state.forEach(track => {
+            track.items.forEach((i, index) => {
+                if(i == item) {
+                    track.items.splice(index, 1);
+                }
+            })
+        });
+
+        this._trackState.next(state);
+    }
+
+    // support drag and drop to specific track
+    public addItem = (item: DashboardItem) => {
+        const state = this._trackState.getValue();
+
+        // check if tracks already contain this component
+        if(state[0].items.indexOf(item) !== -1 || state[1].items.indexOf(item) !== -1) {
+            console.warn('Item with the same id exists on the dashboard.');
+            return;
+        }
+
+        state[0].items.length <= state[1].items.length ? state[0].items.push(item) : state[1].items.push(item);
+
+        this._trackState.next(state);
+    }
+
+}
