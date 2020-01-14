662b33ea81342ea7b926328a90d8c4730aa6741e
Marko Urh <urh.marko@gmail.com>
2020-01-14 13:54:30 +0100

    Dynamic dashboard

--- Frontend/src/app/features/dashboard/dashboard.module.ts
+++ Frontend/src/app/features/dashboard/dashboard.module.ts
@@ -2,6 +2,9 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { DashboardHomeComponent } from './dashboard-home/dashboard-home.component';
+import { DashboardCardContainerComponent } from './dashboard-card-container/dashboard-card-container.component';
+import { TestCardComponent } from './dashboard-card-container/card-components/test-card/test-card.component';
+import { DashboardOutletDirective } from './directives/dashboard-outlet.directive';
 
 
 @NgModule({
@@ -11,12 +14,16 @@ import { DashboardHomeComponent } from './dashboard-home/dashboard-home.componen
     ],
     declarations: [
         DashboardHomeComponent,
+        DashboardOutletDirective,
+        DashboardCardContainerComponent,
+        TestCardComponent,
     ],
     exports: [
     ],
     providers: [
     ],
     entryComponents: [
+        TestCardComponent
     ]
 })
 export class DashboardModule { }
