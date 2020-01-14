6f0d71de246844771dfcd70f286c1ea026d04f8b
Marko Urh <urh.marko@gmail.com>
2020-01-14 13:54:30 +0100

    Dynamic dashboard

---
+++ Frontend/src/app/features/dashboard/dashboard-card-container/dashboard-card-container.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { DashboardItem } from '../models/dashboard-item.interface';
+
+@Component({
+  selector: 'app-dashboard-card-container',
+  templateUrl: './dashboard-card-container.component.html',
+  styleUrls: ['./dashboard-card-container.component.scss']
+})
+export class DashboardCardContainerComponent implements OnInit {
+
+  @Input() item: DashboardItem;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Frontend/src/app/features/dashboard/models/dashboard-cards.ts
@@ -0,0 +1,9 @@
+import { TestCardComponent } from '../dashboard-card-container/card-components/test-card/test-card.component';
+
+export const dashboardCards = {
+    Test: TestCardComponent
+}
+
+export enum DashboardCards {
+    Test = 'Test'
+}
