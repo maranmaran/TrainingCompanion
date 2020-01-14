279249122280992b3f91e7b2d4a6a9b9a3d74a31
Marko Urh <urh.marko@gmail.com>
2020-01-14 14:48:33 +0100

    Endpoint and model for main dashboard tracks

---
+++ Backend/API/Backend.API/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using Backend.Application.Business.Business.Dashboard.GetMainDashboardRequest;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Backend.API.Controllers
+{
+    public class DashboardController : BaseController
+    {
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> GetMainDashboard(Guid userId)
+        {
+            return Ok(await Mediator.Send(new GetMainDashboardRequest(userId)));
+        }
+
+    }
+}
---
+++ Backend/Core/Backend.Domain/Entities/Dashboard/MainDashboard.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Backend.Domain.Entities.Dashboard
+{
+    public class MainDashboard
+    {
+        public Guid Id { get; set; }
+        public IEnumerable<Track> Tracks { get; set; }
+    }
+}
