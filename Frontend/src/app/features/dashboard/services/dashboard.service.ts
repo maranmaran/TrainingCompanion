2660de4f79a83b77118e2ab678de83fdc7c02176
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-01-15 16:34:17 +0100

    no message

--- Frontend/src/app/features/dashboard/services/dashboard.service.ts
+++ Frontend/src/app/features/dashboard/services/dashboard.service.ts
@@ -1,14 +1,13 @@
-import { Injectable, OnInit } from "@angular/core";
-import { Track } from '../models/track.interface';
-import { DashboardCards } from '../models/dashboard-cards';
-import { BehaviorSubject } from 'rxjs';
-import { BaseService } from 'src/business/services/base.service';
 import { HttpClient } from '@angular/common/http';
-import { catchError, take } from 'rxjs/operators';
-import { AppState } from 'src/ngrx/app/app.state';
+import { Injectable, OnInit } from "@angular/core";
 import { Store } from '@ngrx/store';
+import { BehaviorSubject } from 'rxjs';
+import { catchError, take } from 'rxjs/operators';
+import { BaseService } from 'src/business/services/base.service';
+import { AppState } from 'src/ngrx/app/app.state';
 import { currentUserId } from 'src/ngrx/auth/auth.selectors';
 import { DashboardItem } from '../models/dashboard-item.interface';
+import { Track } from '../models/track.interface';
 
 @Injectable({
     providedIn: 'root'
@@ -26,18 +25,11 @@ export class DashboardService extends BaseService implements OnInit {
     private _defaultState: Track[] = [
         {
             items: [
-                {
-                    component: DashboardCards.Test,
-                    id: 'test'
-                }
             ]
         },
         {
             items: [
-                {
-                    component: DashboardCards.Test,
-                    id: 'test'
-                }
+
             ]
         }
     ]
@@ -49,6 +41,8 @@ export class DashboardService extends BaseService implements OnInit {
     ngOnInit() {
         this.store.select(currentUserId).pipe(take(1)).subscribe(id => this._userId = id);
     }
+
+
 
     public getUserTracks() {
         this.http
@@ -79,7 +73,7 @@ export class DashboardService extends BaseService implements OnInit {
 
     public removeItem = (item: DashboardItem) => {
         const state = this._trackState.getValue();
-        
+
         state.forEach(track => {
             track.items.forEach((i, index) => {
                 if(i == item) {
@@ -92,16 +86,16 @@ export class DashboardService extends BaseService implements OnInit {
     }
 
     // support drag and drop to specific track
-    public addItem = (item: DashboardItem) => {
+    public addItem = (item: DashboardItem, trackIdx: number) => {
         const state = this._trackState.getValue();
 
         // check if tracks already contain this component
-        if(state[0].items.indexOf(item) !== -1 || state[1].items.indexOf(item) !== -1) {
-            console.warn('Item with the same id exists on the dashboard.');
+        if(state[trackIdx].items.indexOf(item) !== -1) {
+            console.warn('Item with the same id exists on the dashboard track.');
             return;
         }
 
-        state[0].items.length <= state[1].items.length ? state[0].items.push(item) : state[1].items.push(item);
+        state[trackIdx].items.push(item);
 
         this._trackState.next(state);
     }
