0b7fde007059a0d3e11dba28167fe62e503fca57
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-01-15 23:51:01 +0100

    no message

--- Frontend/src/ngrx/export-import/export-import.reducers.ts
+++ Frontend/src/ngrx/export-import/export-import.reducers.ts
@@ -1,15 +1,56 @@
-import { Action, ActionReducer, createReducer } from '@ngrx/store';
+import { Action, ActionReducer, createReducer, on } from '@ngrx/store';
+import { Guid } from 'guid-typescript';
+import { ExportJob } from 'src/app/features/export-import/models/export-job.model';
+import { ImportResponse } from 'src/server-models/cqrs/import/response/import.response';
+import { ImportJob } from './../../app/features/export-import/models/import-job.model';
+import { ExportResponse } from './../../server-models/cqrs/export/response/export.response';
+import * as ExportImportActions from './export-import.actions';
 import { ExportImportState, initExportImportState } from './export-import.state';
 
 export const exportImportReducers: ActionReducer<ExportImportState, Action> = createReducer(
   initExportImportState,
-)
-  // // CREATE
-  // on(ExportImportActions.exerciseTypeCreated, (state: ExportImportState, payload: { entity: ExerciseType }) => {
-  //     return adapterExerciseType.addOne(payload.entity, state);
-  // }),
 
-  // // UPDATE
-  // on(ExportImportActions.exerciseTypeUpdated, (state: ExportImportState, payload: { entity: Update<ExerciseType> }) => {
-  //     return adapterExerciseType.updateOne(payload.entity, state);
-  // })
+  on(ExportImportActions.addImportJob, (state: ExportImportState, payload: { job: ImportJob }) => {
+    return {
+      ...state,
+      importJobs: [...state.importJobs, payload.job]
+    }
+  }),
+
+  on(ExportImportActions.addExportJob, (state: ExportImportState, payload: { job: ExportJob }) => {
+    return {
+      ...state,
+      exportJobs: [...state.exportJobs, payload.job]
+    }
+  }),
+
+  on(ExportImportActions.removeImportJob, (state: ExportImportState, payload: { id: Guid }) => {
+    return {
+      ...state,
+      importJobs: state.importJobs.filter(job => job.id != payload.id)
+    }
+  }),
+
+  on(ExportImportActions.removeExportJob, (state: ExportImportState, payload: { id: Guid }) => {
+    return {
+      ...state,
+      exportJobs: state.exportJobs.filter(job => job.id != payload.id)
+    }
+  }),
+
+  on(ExportImportActions.setImportResponse, (state: ExportImportState, payload: { response: ImportResponse }) => {
+    return {
+      ...state,
+      lastImportResponse: payload.response
+    }
+  }),
+
+  on(ExportImportActions.setExportResponse, (state: ExportImportState, payload: { response: ExportResponse }) => {
+    return {
+      ...state,
+      lastExportResponse: payload.response
+    }
+  }),
+
+
+)
