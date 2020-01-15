55cf65c10742b492cd38bad48482840751d5deee
Marko Urh <urh.marko@gmail.com>
2020-01-15 13:56:00 +0100

    Singleton services

---
+++ Frontend/src/app/shared/signlar-hubs.module.ts
@@ -0,0 +1,21 @@
+import { SignalrNgChatAdapter } from "../core/ng-chat/signalr-ng-chat-adapter"
+import { NotificationSignalrService } from 'src/business/services/feature-services/notification-signalr.service'
+import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core'
+
+/**
+ * Singleton services - this pattern is used to have singleton instances of services inside lazy loaded modules
+ */
+@NgModule({})
+export class SignalrHubsModule {
+
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: SignalrHubsModule,
+            providers: [
+                SignalrNgChatAdapter,
+                NotificationSignalrService 
+            ]
+        }
+    }
+    
+}
