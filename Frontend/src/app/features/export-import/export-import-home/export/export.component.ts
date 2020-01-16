081b38917735cfe813aaf69f650bcef3093da371
Marko Urh <urh.marko@gmail.com>
2020-01-16 13:44:41 +0100

    no message

--- Frontend/src/app/features/export-import/export-import-home/export/export.component.ts
+++ Frontend/src/app/features/export-import/export-import-home/export/export.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { ExportEntities } from 'src/server-models/enums/export-entities.enum';
 import { ExportService } from 'src/business/services/feature-services/export.service';
+import { Store } from '@ngrx/store';
 import { AppState } from 'src/ngrx/app/app.state';
-import { UIService } from './../../../../../business/services/shared/ui.service';
 
 @Component({
   selector: 'app-export',
@@ -10,11 +10,13 @@ import { UIService } from './../../../../../business/services/shared/ui.service'
   styleUrls: ['./export.component.scss']
 })
 export class ExportComponent implements OnInit {
+  
+  exportEntities = ExportEntities;
+  selectedIExportType: ExportEntities;
 
   constructor(
     private exportService: ExportService,
     private store: Store<AppState>,
-    private UIService: UIService,
   ) { }
 
   ngOnInit() {
