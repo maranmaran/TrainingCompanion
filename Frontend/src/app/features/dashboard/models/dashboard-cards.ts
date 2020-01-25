431d9a2c2298901809cb0dea6420774a1fb397ea
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-01-25 12:12:55 +0100

    Sidebar preview on dashboard

--- Frontend/src/app/features/dashboard/models/dashboard-cards.ts
+++ Frontend/src/app/features/dashboard/models/dashboard-cards.ts
@@ -1,9 +1,13 @@
 import { TestCardComponent } from '../dashboard-home/dashboard-card-container/card-components/test-card/test-card.component';
 
 export const dashboardCards = {
-    Test: TestCardComponent
+  Test: TestCardComponent
+}
+
+export const sidebarCards = {
+  Test: '<h1>test preview</h1>'
 }
 
 export enum DashboardCards {
-    Test = 'Test'
+  Test = 'Test'
 }
--- Frontend/src/app/features/dashboard/models/track.model.ts
+++ Frontend/src/app/features/dashboard/models/track.model.ts
@@ -1,7 +1,6 @@
 import { TrackItem } from './track-item.model';
 
 export class Track {
-
   id: string;
   items: TrackItem[] = [];
 }
