cf8cedba6155df7a97ca25868671eb7bc45d49e1
Marko Urh <urh.marko@gmail.com>
2020-02-10 23:44:02 +0100

    material and layout update

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { HammerModule } from "@angular/platform-browser";
 
 @NgModule({
   imports: [
     CoreModule,
+    HammerModule,
   ],
   declarations: [
     AppComponent,
