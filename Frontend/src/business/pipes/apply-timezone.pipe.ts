ede969b13ca0dbba377395da6e6ebabe172cafa5
Marko Urh <urh.marko@gmail.com>
2020-02-13 16:58:22 +0100

    Moment

--- Frontend/src/business/pipes/apply-timezone.pipe.ts
+++ Frontend/src/business/pipes/apply-timezone.pipe.ts
@@ -8,6 +8,9 @@ export class ApplyTimezonePipe implements PipeTransform {
     // tslint:disable-next-line: ban-types
     transform(value: Object) {
         const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-        return moment(value).tz(timezone).format('L');
+        console.log(value);
+        var date = moment(value).format('L HH:mm');
+        console.log(date);
+        return moment.tz(value, timezone).format('HH:mm');
     }
 }
