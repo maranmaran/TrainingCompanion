1a339e503667d2fb817ce9c303ac8018c4f453de
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-03-02 00:59:16 +0100

    Import / Export - excel library made more lightweight and decoupled

---
+++ Backend/Core/Services/Backend.Service.Excel/Interfaces/IExcelDataReader.cs
@@ -0,0 +1,10 @@
+﻿using OfficeOpenXml;
+using System.Collections.Generic;
+
+namespace Backend.Service.Excel.Interfaces
+{
+    public interface IExcelDataReader<T> where T : class
+    {
+        IEnumerable<T> ReadData(ExcelWorksheet sheet);
+    }
+}
