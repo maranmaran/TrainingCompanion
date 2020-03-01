65c4a0a1aab3b6661db839b51895cf88952103b8
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-03-01 22:12:50 +0100

    no message

---
+++ Frontend/src/app/features/bodyweight/bodyweight-home/bodyweight-create-edit/bodyweight-create-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bodyweight-create-edit',
+  templateUrl: './bodyweight-create-edit.component.html',
+  styleUrls: ['./bodyweight-create-edit.component.scss']
+})
+export class BodyweightCreateEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
