8f28bff6b4ac3d5f237b87bbd8c45b48105539ff
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-03-02 00:59:16 +0100

    Import / Export - excel library made more lightweight and decoupled

--- Backend/Core/Services/Backend.Service.Excel/ExcelService.cs
+++ Backend/Core/Services/Backend.Service.Excel/ExcelService.cs
@@ -1,51 +1,53 @@
-﻿using Backend.Service.Excel.Interfaces;
+﻿using Backend.Common.Extensions;
+using Backend.Service.Excel.Interfaces;
+using Backend.Service.Excel.Utils;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Backend.Service.Excel
 {
     public class ExcelService : IExcelService
     {
-        //public async Task<FileContentResult> Export(IExportDataContainer data, CancellationToken cancellationToken)
-        //{
-        //    var properties = data.GetExportFileProperties();
+        public async Task<FileContentResult> Export(IExcelDataWriter writer, CancellationToken cancellationToken)
+        {
+            var properties = writer.GetProperties();
 
-        //    using var package = new ExcelPackage(new MemoryStream());
-        //    var workbook = package.Workbook.SetProperties(properties);
-        //    var worksheet = workbook.Worksheets.Add(properties.Title);
-        //    var styles = workbook.GetExportTrainingDataColumnNamedStyle();
+            using var package = new ExcelPackage(new MemoryStream());
+            var workbook = package.Workbook.SetProperties(properties);
+            var worksheet = workbook.Worksheets.Add(properties.Title);
 
-        //    data.WriteToSheet(worksheet, styles);
+            writer.Write(worksheet);
 
-        //    worksheet.Calculate();
-        //    worksheet.Cells.AutoFitColumns();
+            worksheet.Calculate();
+            worksheet.Cells.AutoFitColumns();
 
-        //    package.Save();
+            package.Save();
 
-        //    var resultStream = await package.GetResultStream();
-        //    const string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"; // TODO: get from settings or something
+            var resultStream = await package.GetResultStream();
+            const string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"; // TODO: get from settings or something
 
-        //    return new FileContentResult(await resultStream.ToByteArray(cancellationToken: cancellationToken), contentType)
-        //    {
-        //        FileDownloadName = properties.Title
-        //    };
-        //}
+            return new FileContentResult(await resultStream.ToByteArray(cancellationToken: cancellationToken), contentType)
+            {
+                FileDownloadName = properties.Title
+            };
+        }
 
-        //public async Task<IEnumerable<T>> ParseImportData<T>(IFormFile file, CancellationToken cancellationToken) where T : new()
-        //{
-        //    await using var stream = new MemoryStream();
-        //    file.CopyTo(stream);
+        public async Task<IEnumerable<T>> ParseData<T>(IFormFile file, IExcelDataReader<T> reader, CancellationToken cancellationToken) where T : class
+        {
+            await using var stream = new MemoryStream();
+            file.CopyTo(stream);
 
-        //    using var package = new ExcelPackage(stream);
-        //    var worksheet = package.Compatibility.IsWorksheets1Based
-        //        ? package.Workbook.Worksheets[1]
-        //        : package.Workbook.Worksheets[0];
+            using var package = new ExcelPackage(stream);
+            var worksheet = package.Compatibility.IsWorksheets1Based
+                ? package.Workbook.Worksheets[1]
+                : package.Workbook.Worksheets[0];
 
-        //    //return worksheet.ConvertSheetToObjects<T>();
-        //    return null;
-        //}
-
-        //public async Task<IEnumerable<ValidationError>> ValidateData(IFormFile file, IList<ImportColumn> allowedColumns, CancellationToken cancellationToken)
-        //{
-        //    return null; // TODO
-        //}
+            return reader.ReadData(worksheet);
+        }
     }
 }
