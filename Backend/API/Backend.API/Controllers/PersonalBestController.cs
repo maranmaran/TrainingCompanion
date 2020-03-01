60a59a7e40d5c0883c088bb6be6e2d50a9556fe9
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-03-01 13:35:20 +0100

    Bodyweight and personal best controllers

---
+++ Backend/API/Backend.API/Controllers/PersonalBestController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend.Business.ProgressTracking.PersonalBestRequests.Create;
+using Backend.Business.ProgressTracking.PersonalBestRequests.Delete;
+using Backend.Business.ProgressTracking.PersonalBestRequests.GetAll;
+using Backend.Business.ProgressTracking.PersonalBestRequests.Update;
+using Backend.Domain.Entities.ProgressTracking;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.API.Controllers
+{
+    public class PersonalBestController: BaseController
+    {
+        [HttpGet("{exerciseTypeId}")]
+        public async Task<IActionResult> GetAll(Guid exerciseTypeId)
+        {
+            return Ok(await Mediator.Send(new GetAllPersonalBestRequest(exerciseTypeId)));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(PersonalBest entity)
+        {
+            return Ok(await Mediator.Send(new CreatePersonalBestRequest(entity)));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(PersonalBest entity)
+        {
+            return Ok(await Mediator.Send(new UpdatePersonalBestRequest(entity)));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            return Ok(await Mediator.Send(new DeletePersonalBestRequest(id)));
+        }
+    }
+}
--- Backend/Core/Business/Backend.Business.ProgressTracking/BodyweightRequests/Delete/DeleteBodyweightRequest.cs
+++ Backend/Core/Business/Backend.Business.ProgressTracking/BodyweightRequests/Delete/DeleteBodyweightRequest.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using MediatR;
+﻿using MediatR;
+using System;
 
 namespace Backend.Business.ProgressTracking.BodyweightRequests.Delete
 {
     public class DeleteBodyweightRequest : IRequest<Unit>
     {
+        public DeleteBodyweightRequest(Guid id)
+        {
+            Id = id;
+        }
+
         public Guid Id { get; set; }
     }
 }
