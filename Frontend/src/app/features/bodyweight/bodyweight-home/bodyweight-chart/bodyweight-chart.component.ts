b411348c48585583e18cf240923c61232bba4ece
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-03-01 22:12:50 +0100

    no message

---
+++ Frontend/src/app/features/bodyweight/bodyweight-home/bodyweight-chart/bodyweight-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bodyweight-chart',
+  templateUrl: './bodyweight-chart.component.html',
+  styleUrls: ['./bodyweight-chart.component.scss']
+})
+export class BodyweightChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Frontend/src/app/features/bodyweight/bodyweight-home/bodyweight-list/bodyweight-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bodyweight-list',
+  templateUrl: './bodyweight-list.component.html',
+  styleUrls: ['./bodyweight-list.component.scss']
+})
+export class BodyweightListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
