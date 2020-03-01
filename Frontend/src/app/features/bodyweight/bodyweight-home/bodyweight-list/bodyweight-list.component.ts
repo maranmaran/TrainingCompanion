d5708b8bd14372d6ef9775af46c5ca1cc1bc7377
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-03-01 23:10:38 +0100

    no message

--- Frontend/src/app/features/bodyweight/bodyweight-home/bodyweight-list/bodyweight-list.component.ts
+++ Frontend/src/app/features/bodyweight/bodyweight-home/bodyweight-list/bodyweight-list.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { CRUD } from 'src/business/shared/crud.enum';
+import { AppState } from 'src/ngrx/global-setup.ngrx';
+import { Bodyweight } from 'src/server-models/entities/bodyweight.model';
+import { UIService } from './../../../../../business/services/shared/ui.service';
+import { bodyweights } from './../../../../../ngrx/bodyweight/bodyweight.selectors';
+import { BodyweightCreateEditComponent } from './../bodyweight-create-edit/bodyweight-create-edit.component';
 
 @Component({
   selector: 'app-bodyweight-list',
@@ -7,9 +16,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodyweightListComponent implements OnInit {
 
-  constructor() { }
+  bodyweights$: Observable<Bodyweight[]>
+
+  constructor(
+    private uiService: UIService,
+    private store: Store<AppState>
+  ) { }
 
   ngOnInit(): void {
+    this.bodyweights$ = this.store.select(bodyweights)
   }
 
+  onAdd() {
+    const dialogRef = this.uiService.openDialogFromComponent(BodyweightCreateEditComponent, {
+      height: 'auto',
+      width: '98%',
+      maxWidth: '20rem',
+      autoFocus: false,
+      data: { title: 'Add bodyweight record', action: CRUD.Create },
+      panelClass: []
+    })
+
+    dialogRef.afterClosed().pipe(take(1))
+      .subscribe((entity: Bodyweight) => {
+        // something
+        }
+      )
+  }
 }
