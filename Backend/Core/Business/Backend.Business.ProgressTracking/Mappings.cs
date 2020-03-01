e42b75fc6ef0b20790be2f4c89665944cd77b459
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-03-01 23:26:42 +0100

    no message

--- Backend/Core/Business/Backend.Business.ProgressTracking/Mappings.cs
+++ Backend/Core/Business/Backend.Business.ProgressTracking/Mappings.cs
@@ -1,9 +1,15 @@
 ﻿using AutoMapper;
+using Backend.Business.ProgressTracking.BodyweightRequests.Create;
+using Backend.Domain.Entities.ProgressTracking;
 
 namespace Backend.Business.ProgressTracking
 {
     public class Mappings : Profile
     {
+        public Mappings()
+        {
+            CreateMap<CreateBodyweightRequest, Bodyweight>();
+        }
     }
 }
 
