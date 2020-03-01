72774b798f39a725b9ccd18c1f927f8b81559e66
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-03-02 00:59:16 +0100

    Import / Export - excel library made more lightweight and decoupled

---
+++ Backend/Core/Business/Backend.Business.Import/Models/Training/TrainingExcelDataReader.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backend.Business.Import.Models.Training
+{
+    class TrainingExcelDataReader
+    {
+    }
+}
---
+++ Backend/Core/Services/Backend.Service.Excel/Interfaces/IExcelDataWriter.cs
@@ -0,0 +1,11 @@
+﻿using OfficeOpenXml;
+
+namespace Backend.Service.Excel.Interfaces
+{
+    public interface IExcelDataWriter
+    {
+        (string Title, string Author, string Comments, string Company) GetProperties();
+
+        void Write(ExcelWorksheet sheet);
+    }
+}
