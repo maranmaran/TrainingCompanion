2e5df7d5708e27ef2ff58f57a8662bb637f09a7f
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-03-09 21:42:30 +0100

    Bodyweight chart

---
+++ Backend/Core/Business/Backend.Business.Metrics/ReportsRequests/GetBodyweightReport/GetBodyweightReportRequestHandler.cs
@@ -0,0 +1,51 @@
+﻿using Backend.Domain;
+using Backend.Service.Infrastructure.Exceptions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Backend.Business.Reports.ReportsRequests.GetBodyweightReport
+{
+    public class GetBodyweightReportRequestHandler : IRequestHandler<GetBodyweightReportRequest, GetBodyweightReportResponse>
+    {
+        private readonly IApplicationDbContext _context;
+        public GetBodyweightReportRequestHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<GetBodyweightReportResponse> Handle(GetBodyweightReportRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var userSetting =
+                    await _context.UserSettings.FirstOrDefaultAsync(x => x.ApplicationUserId == request.UserId,
+                        cancellationToken);
+
+                if (userSetting == null)
+                    throw new Exception("User setting cannot be null");
+
+                var unitSystem = userSetting.UnitSystem; //TODO do something..
+
+                var data = _context.Bodyweights.Where(x => x.Date >= request.DateFrom && x.Date <= request.DateTo);
+
+                var dates = await data.Select(x => x.Date).ToListAsync(cancellationToken);
+                var values = await data.Select(x => x.Value).ToListAsync(cancellationToken);
+                var result = new GetBodyweightReportResponse()
+                {
+                    Dates = dates,
+                    Values = values
+                };
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                throw new FetchFailureException(nameof(GetBodyweightReportRequest), e);
+            }
+        }
+    }
+}
--- Frontend/src/app/features/bodyweight/bodyweight-home/bodyweight-chart/bodyweight-chart.component.ts
+++ Frontend/src/app/features/bodyweight/bodyweight-home/bodyweight-chart/bodyweight-chart.component.ts
@@ -1,15 +1,98 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MAT_MOMENT_DATE_ADAPTER_OPTIONS, MomentDateAdapter } from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_LOCALE } from '@angular/material/core';
+import { Actions, ofType } from '@ngrx/effects';
+import { Store } from '@ngrx/store';
+import * as moment from 'moment';
+import { combineLatest, Observable } from 'rxjs';
+import { distinctUntilChanged, map, startWith, take } from 'rxjs/operators';
+import { ReportService } from 'src/business/services/feature-services/report.service';
+import { settingsUpdated } from 'src/ngrx/auth/auth.actions';
+import { AppState } from 'src/ngrx/global-setup.ngrx';
+import { activeTheme, isMobile } from 'src/ngrx/user-interface/ui.selectors';
+import { SubSink } from 'subsink';
+import { currentUserId } from './../../../../../ngrx/auth/auth.selectors';
+import { UnitSystem } from './../../../../../server-models/enums/unit-system.enum';
 
 @Component({
   selector: 'app-bodyweight-chart',
   templateUrl: './bodyweight-chart.component.html',
-  styleUrls: ['./bodyweight-chart.component.scss']
+  styleUrls: ['./bodyweight-chart.component.scss'],
+  providers: [
+    ReportService,
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS] }
+  ]
 })
-export class BodyweightChartComponent implements OnInit {
+export class BodyweightChartComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  userId: string;
+  unitSystem = UnitSystem.Metric;
+  isMobile: Observable<boolean>;
 
-  ngOnInit(): void {
+  form: FormGroup;
+
+  subs = new SubSink();
+
+  constructor(
+    private store: Store<AppState>,
+    private actions$: Actions,
+    private reportService: ReportService
+  ) { }
+
+  ngOnInit() {
+
+    this.createForm();
+
+    this.isMobile = this.store.select(isMobile);
+
+    this.subs.add(
+      combineLatest(
+        this.store.select(activeTheme),
+        // listening to this action because first we need unit
+        // system to be stored in db.. then fetch all new calculated data from server
+        this.actions$.pipe(ofType(settingsUpdated), map(val => val.unitSystem), startWith(this.unitSystem)),
+        this.store.select(currentUserId)
+      )
+        .pipe(distinctUntilChanged((a, b) => JSON.stringify(a) == JSON.stringify(b)))
+        .subscribe(val => this.prepareData(val))
+    )
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
+  }
+
+  get dateFrom(): AbstractControl { return this.form.get('dateFrom'); }
+  get dateTo(): AbstractControl { return this.form.get('dateTo'); }
+
+  createForm() {
+    this.form = new FormGroup({
+      dateFrom: new FormControl(moment(new Date()).subtract(1, 'month').toDate(), Validators.required),
+      dateTo: new FormControl(new Date(), Validators.required)
+    });
+  }
+
+  prepareData([theme, system, userId]) {
+    console.log({theme, system, userId});
+    this.userId = userId;
+
+    this.getReportData();
+
+  }
+
+  getReportData() {
+    if(!this.form.valid)
+      return;
+
+    this.reportService.getBodyweightReport(this.userId, this.dateFrom.value, this.dateTo.value)
+    .pipe(take(1))
+    .subscribe(
+      data => {
+        console.log(data)
+      },
+      err => console.log(err)
+    )
   }
 
 }
