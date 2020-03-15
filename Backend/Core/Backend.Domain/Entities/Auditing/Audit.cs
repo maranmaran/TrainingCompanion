2f3593d11a500618066470d9b922e2424cd3bdb6
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-03-15 22:25:35 +0100

    Audit

--- Backend/Core/Backend.Domain/Entities/Auditing/Audit.cs
+++ Backend/Core/Backend.Domain/Entities/Auditing/Audit.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace Backend.Domain.Entities.Auditing
 {
     public class AuditRecord
     {
         public Guid Id { get; set; }
+
         public string Data { get; set; }
+        public AuditData<T> GetData<T>() where T : class
+        {
+            return JsonConvert.DeserializeObject<AuditData<T>>(string.IsNullOrEmpty(Data) ? "{}" : Data);
+        }
+
         public string EntityType { get; set; }
         public string Table { get; set; }
         public string PrimaryKey { get; set; }
@@ -13,5 +20,15 @@ namespace Backend.Domain.Entities.Auditing
         public string Action { get; set; }
         public string User { get; set; }
         public DateTime Date { get; set; }
+    }
+
+    public class AuditData<T> where T : class
+    {
+        public string Table { get; set; }
+        public string Action { get; set; }
+        public Guid PrimaryKey { get; set; }
+        public T Entity { get; set; }
+        public T ColumnValues { get; set; }
+        public bool Valid { get; set; }
     }
 }
