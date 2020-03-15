348e4f71ad105a635ef41dc5c59bc3d3d46a9774
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-03-15 22:25:35 +0100

    Audit

---
+++ Backend/Persistance/Backend.Persistance/Configurations/AuditConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Backend.Domain.Entities.Auditing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Backend.Persistance.Configurations
+{
+    public class AuditConfiguration : IEntityTypeConfiguration<AuditRecord>
+    {
+        public void Configure(EntityTypeBuilder<AuditRecord> builder)
+        {
+
+        }
+    }
+}
