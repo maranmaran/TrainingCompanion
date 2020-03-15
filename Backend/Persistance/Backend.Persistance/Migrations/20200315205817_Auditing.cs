134de45d50b598c691d943126ac21f07d85208af
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-03-15 22:25:35 +0100

    Audit

---
+++ Backend/Persistance/Backend.Persistance/Migrations/20200315205817_Auditing.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace Backend.Persistance.Migrations
+{
+    public partial class Auditing : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.CreateTable(
+                name: "Audits",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Data = table.Column<string>(nullable: true),
+                    EntityType = table.Column<string>(nullable: true),
+                    Table = table.Column<string>(nullable: true),
+                    PrimaryKey = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    Action = table.Column<string>(nullable: true),
+                    User = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Audits", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Audits");
+        }
+    }
+}
