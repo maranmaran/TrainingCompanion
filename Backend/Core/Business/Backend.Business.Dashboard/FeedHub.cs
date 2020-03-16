777c737a80fac5d2a789a48bd05df871220409ea
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-03-16 23:40:49 +0100

    Init real time feed

---
+++ Backend/Core/Business/Backend.Business.Dashboard/FeedHub.cs
@@ -0,0 +1,21 @@
+﻿using Backend.Business.Dashboard.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Backend.Business.Dashboard
+{
+    public interface IFeedHub
+    {
+        Task PushFeedActivity(Activity activity);
+    }
+
+    [Authorize]
+    public class FeedHub : Hub<IFeedHub>
+    {
+        public FeedHub()
+        {
+        }
+    }
+
+}
--- Backend/Persistance/Backend.Persistance/ServiceCollectionExtensions.cs
+++ Backend/Persistance/Backend.Persistance/ServiceCollectionExtensions.cs
@@ -1,10 +1,16 @@
 ﻿using Audit.Core;
+using Backend.Business.Dashboard;
+using Backend.Business.Dashboard.Models;
+using Backend.Business.Users.UsersRequests.GetUser;
 using Backend.Domain.Entities.Auditing;
 using Backend.Domain.Entities.Exercises;
 using Backend.Domain.Entities.Media;
 using Backend.Domain.Entities.ProgressTracking;
 using Backend.Domain.Entities.TrainingLog;
+using Backend.Domain.Enum;
+using MediatR;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Linq;
@@ -13,6 +19,8 @@ namespace Backend.Persistance
 {
     public static partial class ServiceCollectionExtensions
     {
+
+        //TODO Move all this to Dashboard project..because thats BoundedContext for FEED
         public static void ConfigureAuditEfCore(this IServiceCollection services)
         {
             Audit.EntityFramework.Configuration.Setup()
@@ -43,19 +51,38 @@ namespace Backend.Persistance
             Configuration.Setup()
                 .UseEntityFramework(_ => _
                     .AuditTypeMapper(t => typeof(AuditRecord))
-                    .AuditEntityAction<AuditRecord>((ev, entry, entity) =>
-                    {
-                        Guid.TryParse(ev.CustomFields["UserId"].ToString(), out var userId);
-                        entity.UserId = userId;
+                    .AuditEntityAction<AuditRecord>(async (ev, entry, entity) =>
+                   {
+                       //TODO method for mapping
 
-                        entity.Data = entry.ToJson();
-                        entity.EntityType = entry.EntityType.Name;
-                        entity.Date = DateTime.Now;
-                        entity.PrimaryKey = entry.PrimaryKey.First().Value.ToString();
-                        entity.Table = entry.Table;
-                        entity.Action = entry.Action;
-                        entity.Date = DateTime.Now;
-                    })
+                       Guid.TryParse(ev.CustomFields["UserId"].ToString(), out var userId);
+                       entity.UserId = userId;
+
+                       entity.Data = entry.ToJson();
+                       entity.EntityType = entry.EntityType.Name;
+                       entity.Date = DateTime.Now;
+                       entity.PrimaryKey = entry.PrimaryKey.First().Value.ToString();
+                       entity.Table = entry.Table;
+                       entity.Action = entry.Action;
+                       entity.Date = DateTime.Now;
+
+
+                       //TODO method for pushing to feed in real time
+                       var provider = services.BuildServiceProvider();
+                       var ctx = provider.GetService<IHttpContextAccessor>();
+                       var hub = ctx.HttpContext.RequestServices.GetRequiredService<IHubContext<FeedHub, IFeedHub>>();
+                       var mediator = ctx.HttpContext.RequestServices.GetRequiredService<IMediator>();
+
+                       var activity = new Activity()
+                       {
+                           Date = entity.Date,
+                           Type = (ActivityType)Enum.Parse(typeof(ActivityType), entity.EntityType, true),
+                           UserId = entity.UserId,
+                           UserName = (await mediator.Send(new GetUserRequest(entity.UserId, AccountType.Athlete))).FullName
+                       };
+
+                       await hub.Clients.All.PushFeedActivity(activity);
+                   })
                     .IgnoreMatchedProperties(true));
         }
     }
