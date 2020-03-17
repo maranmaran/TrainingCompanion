4a20e5c19ecce0d09b4a32a6b324aefea8b91a2c
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-03-17 22:59:20 +0100

    no message

--- Frontend/src/app/shared/export-import-services.module.ts
+++ Frontend/src/app/shared/export-import-services.module.ts
@@ -1,7 +1,7 @@
 import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 import { ExportService } from 'src/business/services/feature-services/export.service';
 import { ImportService } from 'src/business/services/feature-services/import.service';
-import { SignalrHubsModule } from './signlar-hubs.module';
+import { SignalrHubsModule } from './signalr-hubs.module';
 /**
  * Singleton services - this pattern is used to have singleton instances of services inside lazy loaded modules
  */
