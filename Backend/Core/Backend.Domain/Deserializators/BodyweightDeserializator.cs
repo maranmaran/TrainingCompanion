332e0f94bafea1b2deb89c03a8161cdbbcc01afe
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-03-21 17:42:01 +0100

    no message

---
+++ Backend/Core/Backend.Domain/Deserializators/BodyweightDeserializator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Backend.Domain.Deserializators
+{
+    public class BodyweightDeserializer
+    {
+        public string Id { get; set; }
+        public double Value { get; set; }
+        public DateTime Date { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
