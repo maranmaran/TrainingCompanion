306a859164dee8112b38e19397265ab96b3d78fa
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-03-21 17:42:01 +0100

    no message

---
+++ Backend/Core/Business/Backend.Business.Dashboard/ActivityHelper.cs
@@ -0,0 +1,32 @@
+﻿using Backend.Domain;
+using Backend.Domain.Deserializators;
+using Backend.Domain.Entities.Auditing;
+using Backend.Domain.Entities.Media;
+using Backend.Domain.Entities.ProgressTracking;
+using Backend.Domain.Entities.TrainingLog;
+using Backend.Domain.Entities.User;
+using System;
+
+namespace Backend.Business.Dashboard
+{
+    public static class ActivityHelper
+    {
+        public static string GetPayload(AuditRecord audit, UserSetting settings)
+        {
+            switch (audit.EntityType)
+            {
+                case nameof(Training):
+                    return "added new training";
+                case nameof(MediaFile):
+                    return "attached new media";
+                case nameof(Bodyweight):
+                    return $"logged bodyweight of {audit.GetData<BodyweightDeserializer>().Entity.Value.FromMetricTo(settings.UnitSystem)} {settings.UnitSystem.GetUnitLabel()}";
+                case nameof(PersonalBest):
+                    return "has new PR!";
+                default:
+                    throw new ArgumentException($"Entity type is not recognized. {audit.EntityType}");
+            }
+        }
+
+    }
+}
