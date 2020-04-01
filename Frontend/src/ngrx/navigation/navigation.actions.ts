a5ee5f50d38b08c71ce5797e420dbabbff2c41a6
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-04-01 14:12:12 +0200

    Navigation state

--- Frontend/src/ngrx/navigation/navigation.actions.ts
+++ Frontend/src/ngrx/navigation/navigation.actions.ts
@@ -1,5 +1,6 @@
+import { createAction, props } from '@ngrx/store';
+import { AthleteBottomNavigation } from './navigation.state';
 
-// export const athleteCreated = createAction(
-//   '[Athlete] Created',
-//   props<{ entity: ApplicationUser }>()
-// );
+export const athleteActiveBottomNav = createAction('[Navigation - Athlete] Set active nav', props<{ nav: AthleteBottomNavigation }>());
+export const athleteActiveTab = createAction('[Navigation - Athlete] Set active tab', props<{ tab: number }>());
+export const athletePreviousRoute = createAction('[Navigation - Athlete] Set previous route', props<{ route: string }>());
--- Frontend/src/ngrx/navigation/navigation.state.ts
+++ Frontend/src/ngrx/navigation/navigation.state.ts
@@ -1,8 +1,23 @@
+export enum AthleteBottomNavigation {
+  Main = 0,
+  Chat = 1,
+  Dashboard = 2,
+  TrainingLog = 3,
+  BodyweightLog = 4
+}
 
 export interface NavigationState {
+  // athlete routes and navigation (tabs)
+  athleteActiveNavigation: AthleteBottomNavigation,
+  athleteActiveTabIdx: number;
+  athletePreviousRoute: string;
 }
 
 export const navigationInitialState: NavigationState = {
+  athleteActiveNavigation: AthleteBottomNavigation.Main,
+  athleteActiveTabIdx: 0,
+  athletePreviousRoute: undefined
 }
+
 
 
