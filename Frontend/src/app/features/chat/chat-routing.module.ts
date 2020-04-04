ffb174b9f1d7b2f2d21c3d3077a6adf02b0be278
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-04-04 22:09:10 +0200

    Athlete pov work in progress with tabs

--- Frontend/src/app/features/chat/chat-routing.module.ts
+++ Frontend/src/app/features/chat/chat-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatBodyComponent } from './components/chat-fullscreen/chat-body/chat-body.component';
 import { ChatFullscreenComponent } from './components/chat-fullscreen/chat-fullscreen.component';
+import { FriendListComponent } from './components/chat-fullscreen/friend-list/friend-list.component';
 
 const routes: Routes = [
     { path: '', component: ChatFullscreenComponent, resolve: { }},
-    // { path: 'friends', component: FriendListComponent, resolve: { }},
-    // { path: 'messages', component: ChatBodyRouteComponent, resolve: { }},
+    { path: 'friends', component: FriendListComponent, resolve: { }},
+    { path: 'messages', component: ChatBodyComponent, resolve: { }},
     { path: '**', redirectTo: '/' }, //always last
 ];
 
