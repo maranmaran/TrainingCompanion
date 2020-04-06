48ea18a49baef463f45ec0ba700662d900638681
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-04-06 18:52:12 +0200

    no message

--- Frontend/src/ngrx/chat/chat.selectors.ts
+++ Frontend/src/ngrx/chat/chat.selectors.ts
@@ -4,6 +4,11 @@ import { ChatState } from './chat.state';
 
 export const selectChatState = createFeatureSelector<ChatState>("chat");
 
+export const isFullScreenChatActive = createSelector(
+    selectChatState,
+    (chatState: ChatState) => chatState.fullscreenChatActive
+);
+
 export const friends = createSelector(
   selectChatState,
   (chatState: ChatState) => chatState.friends
@@ -11,10 +16,6 @@ export const friends = createSelector(
 export const friendsMetadata = createSelector(
   selectChatState,
   (chatState: ChatState) => chatState.friendsMetadata
-);
-export const isFullScreenChatActive = createSelector(
-    selectChatState,
-    (chatState: ChatState) => chatState.fullscreenChatActive
 );
 
 export const selectedFriend = createSelector(
@@ -25,11 +26,21 @@ export const selectedFriendWindow = createSelector(
   selectChatState,
   (chatState: ChatState) => _.cloneDeep(chatState.windows[chatState.selectedFriend.id])
 );
+
+
+
 export const totalUnreadChatMessages = createSelector(
   selectChatState,
   (chatState: ChatState) => chatState.friendsMetadata.reduce((prev, cur) => prev + cur.totalUnreadMessages, 0)
 )
-export const chatMessages = createSelector(
+export const totalUnreadChatMessagesForFriend = createSelector(
   selectChatState,
-  (chatState: ChatState) => chatState.messages
+  (chatState: ChatState, id: string) => {
+    let friendIdx = chatState.friends.findIndex(x => x.id == id);
+    return chatState.friendsMetadata[friendIdx].totalUnreadMessages;
+  }
+)
+export const totalUnreadChatMessagesByFriend = createSelector(
+  selectChatState,
+  (chatState: ChatState) => chatState.friendsMetadata.map(x => x.totalUnreadMessages)
 )
--- Frontend/src/ngrx/chat/chat.state.ts
+++ Frontend/src/ngrx/chat/chat.state.ts
@@ -1,22 +1,19 @@
-import { Message } from 'src/app/features/chat/models/message.model';
 import { Window } from 'src/app/features/chat/models/window.model';
 import { IChatParticipant } from './../../app/features/chat/models/chat-participant.model';
 import { ParticipantMetadata } from './../../app/features/chat/models/participant-metadata.model';
 
 export interface ChatState {
+    fullscreenChatActive: boolean,
     windows: {[key: string]: Window},
     friends: IChatParticipant[],
     friendsMetadata: ParticipantMetadata[],
     selectedFriend: IChatParticipant,
-    fullscreenChatActive: boolean,
-    messages: Message[]
 }
 
 export const initialChatState: ChatState = {
+    fullscreenChatActive: false,
     windows: undefined,
     friends: undefined,
     friendsMetadata: undefined,
     selectedFriend: undefined,
-    fullscreenChatActive: false,
-    messages: undefined
 };
