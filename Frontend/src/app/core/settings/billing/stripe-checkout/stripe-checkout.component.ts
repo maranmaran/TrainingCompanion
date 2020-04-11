28f3c3c61fdd629a36d1f6115450bc95afc4af23
Marko Urh <urh.marko@gmail.com>
2020-04-11 05:18:55 +0200

    Flags

--- Frontend/src/app/core/settings/billing/stripe-checkout/stripe-checkout.component.ts
+++ Frontend/src/app/core/settings/billing/stripe-checkout/stripe-checkout.component.ts
@@ -1,50 +1,27 @@
+import { BillingService } from 'src/business/services/feature-services/billing.service';
 import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
 import { Element as StripeElement, ElementOptions, ElementsOptions, StripeCardComponent, StripeService } from 'ngx-stripe';
-import { Observable } from 'rxjs';
-import { map, startWith, take } from 'rxjs/operators';
+import { Observable, observable, of } from 'rxjs';
+import { map, startWith, take, catchError } from 'rxjs/operators';
 import { Theme } from 'src/business/shared/theme.enum';
 import { currentUser } from 'src/ngrx/auth/auth.selectors';
 import { AppState } from 'src/ngrx/global-setup.ngrx';
 import { CurrentUser } from 'src/server-models/cqrs/authorization/current-user.response';
 import { SubSink } from 'subsink';
+import { Country } from 'src/business/shared/models/country.model';
 
 @Component({
   selector: 'app-stripe-checkout',
   templateUrl: './stripe-checkout.component.html',
-  styleUrls: ['./stripe-checkout.component.scss']
+  styleUrls: ['./stripe-checkout.component.scss'],
+  providers: [BillingService]
 })
 export class StripeCheckoutComponent implements OnInit, OnDestroy {
 
-  countries: string[] = ["Afghanistan", "Albania", "Algeria", "AmericanSamoa", "Andorra", "Angola",
-    "Anguilla", "Antarctica", "AntiguaandBarbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan",
-    "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia",
-    "BosniaandHerzegovina", "Botswana", "BouvetIsland", "Brazil", "BritishIndianOceanTerritory", "BruneiDarussalam", "Bulgaria",
-    "BurkinaFaso", "Burundi", "Cambodia", "Cameroon", "Canada", "CapeVerde", "CaymanIslands", "CentralAfricanRepublic", "Chad",
-    "Chile", "China", "ChristmasIsland", "Cocos(Keeling)Islands", "Colombia", "Comoros", "Congo", "Congo,TheDemocraticRepublicofThe",
-    "CookIslands", "CostaRica", "CoteD'ivoire", "Croatia", "Cuba", "Cyprus", "CzechRepublic", "Denmark", "Djibouti", "Dominica", "DominicanRepublic",
-    "Ecuador", "Egypt", "ElSalvador", "EquatorialGuinea", "Eritrea", "Estonia", "Ethiopia", "FalklandIslands(Malvinas)", "FaroeIslands",
-    "Fiji", "Finland", "France", "FrenchGuiana", "FrenchPolynesia", "FrenchSouthernTerritories", "Gabon", "Gambia", "Georgia", "Germany",
-    "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea-bissau", "Guyana",
-    "Haiti", "HeardIslandandMcdonaldIslands", "HolySee(VaticanCityState)", "Honduras", "HongKong", "Hungary", "Iceland", "India", "Indonesia",
-    "Iran,IslamicRepublicof", "Iraq", "Ireland", "IsleofMan", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya",
-    "Kiribati", "Korea,DemocraticPeople'sRepublicof", "Korea,Republicof", "Kuwait", "Kyrgyzstan", "LaoPeople'sDemocraticRepublic", "Latvia",
-    "Lebanon", "Lesotho", "Liberia", "LibyanArabJamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macao", "Macedonia,TheFormerYugoslavRepublicof",
-    "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "MarshallIslands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico",
-    "Micronesia,FederatedStatesof", "Moldova,Republicof", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Myanmar",
-    "Namibia", "Nauru", "Nepal", "Netherlands", "NetherlandsAntilles", "NewCaledonia", "NewZealand", "Nicaragua", "Niger", "Nigeria", "Niue",
-    "NorfolkIsland", "NorthernMarianaIslands", "Norway", "Oman", "Pakistan", "Palau", "PalestinianTerritory,Occupied", "Panama", "PapuaNewGuinea",
-    "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "PuertoRico", "Qatar", "Reunion", "Romania", "RussianFederation", "Rwanda",
-    "SaintHelena", "SaintKittsandNevis", "SaintLucia", "SaintPierreandMiquelon", "SaintVincentandTheGrenadines", "Samoa", "SanMarino", "SaoTomeandPrincipe",
-    "SaudiArabia", "Senegal", "Serbia", "Seychelles", "SierraLeone", "Singapore", "Slovakia", "Slovenia", "SolomonIslands", "Somalia", "SouthAfrica",
-    "SouthGeorgiaandTheSouthSandwichIslands", "Spain", "SriLanka", "Sudan", "Suriname", "SvalbardandJanMayen", "Swaziland", "Sweden", "Switzerland",
-    "SyrianArabRepublic", "Taiwan,ProvinceofChina", "Tajikistan", "Tanzania,UnitedRepublicof", "Thailand", "Togo", "Tokelau", "Tonga", "TrinidadandTobago"
-    , "Tunisia", "Turkey", "Turkmenistan", "TurksandCaicosIslands", "Tuvalu", "U.S.VirginIslands,U.S.", "Uganda", "Ukraine", "UnitedArabEmirates",
-    "UnitedKingdom", "UnitedStates", "UnitedStatesMinorOutlyingIslands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "VietNam", "VirginIslands,British",
-    "WallisandFutuna", "WesternSahara", "Yemen", "Zambia", "Zimbabwe", "ÅlandIslands"];
-
+  countries: Country[];
   error: any;
   complete: Function;
   element: StripeElement;
@@ -76,8 +53,14 @@ export class StripeCheckoutComponent implements OnInit, OnDestroy {
   constructor(
     private _stripe: StripeService,
     private dialogRef: MatDialogRef<StripeCheckoutComponent>,
-    private store: Store<AppState>
+    private store: Store<AppState>,
+    private _billingService: BillingService
   ) {
+  }
+
+
+
+  ngOnInit() {
 
     this.store.select(currentUser).pipe(take(1))
       .subscribe(user => {
@@ -90,23 +73,25 @@ export class StripeCheckoutComponent implements OnInit, OnDestroy {
         }
 
         this.setCardComponentStyling();
+        this.createForm();
       });
 
+    this._billingService.getCountries().pipe(take(1))
+      .subscribe(
+        countries => (this.countries = countries as Country[], this.getCountries()),
+        err => console.log(err)
+      );
+
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
   }
 
   setCardComponentStyling() {
     this.cardOptions.style.base.color = this.cardComponentColors.font;
     this.cardOptions.style.base.iconColor = this.cardComponentColors.font;
-    this.cardOptions.style.base["::placeholder"].color = this.cardComponentColors.font;
-  }
-
-  ngOnInit() {
-    this.createForm();
-    this.getCountries();
-  }
-
-  ngOnDestroy() {
-    this.subs.unsubscribe();
+    // this.cardOptions.style.base["::placeholder"].color = this.cardComponentColors.font;
   }
 
   createForm() {
@@ -127,7 +112,7 @@ export class StripeCheckoutComponent implements OnInit, OnDestroy {
   get state(): AbstractControl { return this.checkoutForm.get('state'); }
   get country(): AbstractControl { return this.checkoutForm.get('country'); }
 
-  filteredCountries: Observable<string[]>;
+  filteredCountries: Observable<Country[]>;
   getCountries() {
     this.filteredCountries = this.country.valueChanges
       .pipe(
@@ -136,11 +121,10 @@ export class StripeCheckoutComponent implements OnInit, OnDestroy {
       );
   }
 
-  private _filter(value: string): string[] {
+  private _filter(value: string): Country[] {
     const filterValue = value.toString().toLowerCase();
-    return this.countries.filter(option => option.toLowerCase().includes(filterValue));
+    return this.countries.filter(option => option.name.toLowerCase().includes(filterValue));
   }
-
 
   @ViewChild(StripeCardComponent, { static: true }) card: StripeCardComponent;
   getCardToken() {
@@ -178,8 +162,7 @@ export class StripeCheckoutComponent implements OnInit, OnDestroy {
     this.error = 'Something went wrong please update card information';
   }
 
-
-  public onClose() {
+  onClose() {
     this.dialogRef.close('Closed');
   }
 
