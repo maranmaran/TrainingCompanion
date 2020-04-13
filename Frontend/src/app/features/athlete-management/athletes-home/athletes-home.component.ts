5062fb1a3e6a84afd90a434147fdaba369a79040
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-04-13 13:04:30 +0200

    I18n

--- Frontend/src/app/features/athlete-management/athletes-home/athletes-home.component.ts
+++ Frontend/src/app/features/athlete-management/athletes-home/athletes-home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { TranslateService } from '@ngx-translate/core';
+import { take } from 'rxjs/operators';
+import { language } from 'src/ngrx/user-interface/ui.selectors';
+import { UIState } from './../../../../ngrx/user-interface/ui.state';
 
 @Component({
   selector: 'app-athletes-home',
@@ -8,7 +13,11 @@ import { Component, OnInit } from '@angular/core';
 export class AthletesHomeComponent implements OnInit {
 
   constructor(
-  ) { }
+    private translateService: TranslateService,
+    private store: Store<UIState>
+  ) {
+    this.store.select(language).pipe(take(1)).subscribe(lang => this.translateService.use("hr"));
+  }
 
   ngOnInit() {
   }
--- Frontend/src/app/features/athlete-management/athletes.module.ts
+++ Frontend/src/app/features/athlete-management/athletes.module.ts
@@ -1,16 +1,19 @@
+import { HttpBackend } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { AthleteManagementHttpLoaderFactory } from 'src/assets/i18n/translation-http-loader.factory';
+import { UIService } from 'src/business/services/shared/ui.service';
 import { athletesReducer } from 'src/ngrx/athletes/athlete.reducers';
 import { AthletesResolver } from '../../../business/resolvers/athletes.resolver';
 import { AthletesEffects } from '../../../ngrx/athletes/athlete.effects';
-import { AthleteDetailsComponent } from './athletes-home/athlete-details/athlete-details.component';
-import { AthletesHomeComponent } from './athletes-home/athletes-home.component';
-import { AthleteListComponent } from './athletes-home/athlete-list/athlete-list.component';
-import { AthletesRoutingModule } from './athletes-routing.module';
 import { AthleteCreateEditComponent } from './athletes-home/athlete-create-edit/athlete-create-edit.component';
-import { UIService } from 'src/business/services/shared/ui.service';
+import { AthleteDetailsComponent } from './athletes-home/athlete-details/athlete-details.component';
+import { AthleteListComponent } from './athletes-home/athlete-list/athlete-list.component';
+import { AthletesHomeComponent } from './athletes-home/athletes-home.component';
+import { AthletesRoutingModule } from './athletes-routing.module';
 
 
 @NgModule({
@@ -18,7 +21,17 @@ import { UIService } from 'src/business/services/shared/ui.service';
         SharedModule,
         AthletesRoutingModule,
         StoreModule.forFeature('athletes', athletesReducer),
-        EffectsModule.forFeature([AthletesEffects])
+        EffectsModule.forFeature([AthletesEffects]),
+        // lazy loaded modules import of translate module
+        // https://github.com/ngx-translate/core#1-import-the-translatemodule
+        TranslateModule.forChild({
+          isolate: true,
+          loader: {
+            provide: TranslateLoader,
+            useFactory: AthleteManagementHttpLoaderFactory,
+            deps: [HttpBackend]
+          }
+        })
     ],
     declarations: [
         AthletesHomeComponent,
