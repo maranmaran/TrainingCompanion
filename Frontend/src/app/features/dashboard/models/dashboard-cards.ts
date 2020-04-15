8e9ba1ddffd0edb6e2f7e7be5e5fc3028e38b475
Marko Urh <urh.marko@gmail.com>
2020-04-15 21:00:55 +0200

    Charts

--- Frontend/src/app/features/dashboard/models/dashboard-cards.ts
+++ Frontend/src/app/features/dashboard/models/dashboard-cards.ts
@@ -11,17 +11,6 @@ export const dashboardCards = {
   Volume: VolumeCardComponent,
 }
 
-export const sidebarCards = {
-  Volume: `
-          <h4>Track exercises volume: </h4>
-          <i class="fas fa-chart-pie fa-5x row-center additional-padding"></i>
-        `,
-  Max: `
-          <h4>Track exercises volume: </h4>
-          <i class="fas fa-chart-pie fa-5x row-center additional-padding"></i>
-        `,
-}
-
 export const mainDashboardComponents: TrackItem[] = [
   { component: DashboardCards.Volume, code: 'dashboard-volume', id: Guid.EMPTY, jsonParams: null },
   { component: DashboardCards.Max, code: 'dashboard-max', id: Guid.EMPTY, jsonParams: null },
