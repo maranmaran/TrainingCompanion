397cdf3ebadedba608088c67610544dd56a29663
Marko Urh <urh.marko@gmail.com>
2020-04-18 02:15:11 +0200

    Init create of component

---
+++ Backend/Core/Business/Backend.Business.TrainingPrograms/Mappings.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Backend.Business.TrainingPrograms.TrainingProgramRequests.Create;
+using Backend.Business.TrainingPrograms.TrainingProgramRequests.Update;
+using Backend.Domain.Entities.TrainingProgramMaker;
+
+namespace Backend.Business.TrainingPrograms
+{
+    public class Mappings : Profile
+    {
+        public Mappings()
+        {
+            CreateMap<CreateTrainingProgramRequest, TrainingProgram>();
+            CreateMap<UpdateTrainingProgramRequest, TrainingProgram>();
+        }
+
+    }
+}
