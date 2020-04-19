e9841bae8327fc3a39373de2ce55179d725a1826
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-04-19 21:41:52 +0200

    Training, exercises and set components

---
+++ Frontend/src/app/features/training-program/training-program-home/block-exercise-components/block-exercise-list/block-training-exercise-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-block-exercise-list',
+  templateUrl: './block-exercise-list.component.html',
+  styleUrls: ['./block-exercise-list.component.scss']
+})
+export class BlockExerciseListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Frontend/src/app/features/training-program/training-program-home/block-training-components/block-training-details/block-training-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-block-training-details',
+  templateUrl: './block-training-details.component.html',
+  styleUrls: ['./block-training-details.component.scss']
+})
+export class BlockTrainingDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
