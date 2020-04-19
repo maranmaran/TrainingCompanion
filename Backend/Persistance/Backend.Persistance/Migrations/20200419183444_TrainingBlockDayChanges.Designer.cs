6c50e4bb46f84ffa1fc24d0b8a7b212487934121
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-04-19 20:58:47 +0200

    Training block day list

--- Backend/Persistance/Backend.Persistance/Migrations/20200419183444_TrainingBlockDayChanges.Designer.cs
+++ Backend/Persistance/Backend.Persistance/Migrations/20200419183444_TrainingBlockDayChanges.Designer.cs
@@ -5,14 +5,16 @@ using Backend.Persistance;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Backend.Persistance.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200419183444_TrainingBlockDayChanges")]
+    partial class TrainingBlockDayChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
