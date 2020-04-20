43250080a70056945321aa1857ac81e3c0ddd7ed
LAPTOP-3J6AH590\Marko Urh <urh.marko@gmail.com>
2020-04-20 23:33:48 +0200

    no message

---
+++ Frontend/src/app/features/training-log/training/training.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { TrainingDetailsResolver } from 'src/business/resolvers/training-details.resolver';
+import { ReportService } from 'src/business/services/feature-services/report.service';
+import { TrainingService } from 'src/business/services/feature-services/training.service';
+import { UIService } from 'src/business/services/shared/ui.service';
+import { TrainingCreateEditComponent } from './training-create-edit/training-create-edit.component';
+import { TrainingMediaComponent } from './training-details/panels/training-media/training-media.component';
+import { TrainingMetricsComponent } from './training-details/panels/training-metrics/training-metrics.component';
+import { TrainingNoteComponent } from './training-details/panels/training-note/training-note.component';
+import { TrainingDetailsDataComponent } from './training-details/training-details-data/training-details-data.component';
+import { TrainingDetailsComponent } from './training-details/training-details.component';
+import { TrainingListComponent } from './training-list/training-list.component';
+import { TrainingMonthComponent } from './training-month/training-month.component';
+
+
+@NgModule({
+  imports: [
+      SharedModule,
+  ],
+  declarations: [
+      TrainingDetailsDataComponent,
+      TrainingDetailsComponent,
+      TrainingNoteComponent,
+      TrainingMetricsComponent,
+      TrainingMediaComponent,
+      TrainingCreateEditComponent,
+      TrainingListComponent,
+      TrainingMonthComponent,
+  ],
+  exports: [
+    TrainingListComponent,
+  ],
+  providers: [
+      UIService,
+      TrainingService,
+      ReportService,
+      TrainingDetailsResolver,
+  ],
+  entryComponents: [
+  ]
+})
+export class TrainingModule {}
--- Frontend/src/app/features/training-program/training-program-home/training-program-home.component.ts
+++ Frontend/src/app/features/training-program/training-program-home/training-program-home.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { combineLatest } from 'rxjs';
+import { AppState } from 'src/ngrx/global-setup.ngrx';
+import { selectedTrainingBlockDayId } from 'src/ngrx/training-program/training-block-day/training-block-day.selectors';
+import { selectedTrainingProgramId } from 'src/ngrx/training-program/training-program/training-program.selectors';
 import { SubSink } from 'subsink';
+import { selectedTrainingBlockId } from './../../../../ngrx/training-program/training-block/training-block.selectors';
 
 @Component({
   selector: 'app-training-program-home',
@@ -10,10 +16,26 @@ export class TrainingProgramHomeComponent implements OnInit {
 
   private _subs = new SubSink();
 
+  programSelected: boolean
+  blockSelected: boolean
+  blockDaySelected: boolean
+
   constructor(
+    private store: Store<AppState>
   ) { }
 
   ngOnInit(): void {
+    this._subs.add(
+      combineLatest(
+        this.store.select(selectedTrainingProgramId),
+        this.store.select(selectedTrainingBlockId),
+        this.store.select(selectedTrainingBlockDayId),
+      ).subscribe(([program, block, blockDay]) => {
+        this.programSelected = !!program;
+        this.blockSelected = !!program && !!block;
+        this.blockDaySelected = !!program && !!block && !!blockDay;
+      })
+    );
   }
 
 }
