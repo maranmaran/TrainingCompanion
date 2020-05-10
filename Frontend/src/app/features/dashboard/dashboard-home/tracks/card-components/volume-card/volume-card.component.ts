cad51fdedfa9ca1f887a809c8d6c127c4f203ece
Marko Urh <urh.marko@gmail.com>
2020-05-10 15:49:01 +0200

    Fixed date spans instead of intervals. But also written so it can be used in reporting for example.. Will need more work probably

--- Frontend/src/app/features/dashboard/dashboard-home/tracks/card-components/volume-card/volume-card.component.ts
+++ Frontend/src/app/features/dashboard/dashboard-home/tracks/card-components/volume-card/volume-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, Output, EventEmitter } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MAT_MOMENT_DATE_ADAPTER_OPTIONS, MomentDateAdapter } from '@angular/material-moment-adapter';
 import { DateAdapter, MAT_DATE_LOCALE } from '@angular/material/core';
@@ -32,6 +32,63 @@ import { UnitSystem } from './../../../../../../../server-models/enums/unit-syst
 import { PagingModel } from './../../../../../../shared/material-table/table-models/paging.model';
 import { GetVolumeCardChartConfig } from './volume-card-chart.config';
 
+// Options to choose from when on dashboard graph - fixed moving window date intervals
+export const dateSpansDict = {
+  0: { label: 'Last week', dateFrom: moment((new Date())).subtract(1, 'week').toDate() },
+  1: { label: 'Last month', dateFrom: moment((new Date())).subtract(1, 'month').toDate() },
+  2: { label: 'Last 3 months', dateFrom: moment((new Date())).subtract(3, 'month').toDate() },
+  3: { label: 'Last 5 months', dateFrom: moment((new Date())).subtract(5, 'month').toDate() },
+}
+
+// Types of options for moving window date spans
+export enum CardDateSpan {
+  Week,
+  Month,
+  ThreeMonths,
+  FiveMonths
+}
+
+// carries all data for dashboard cards 
+export class CardParameters {
+
+  constructor(jsonParams: string, setSpan: boolean) {
+
+    if (!jsonParams) return; // default
+
+    const params = JSON.parse(jsonParams);
+
+    if (setSpan) {
+
+      if (moment(params.dateTo).diff(moment(params.dateFrom), 'days') == 7) {
+        params.dateSpanType = CardDateSpan.Week;
+      } else {
+        switch (moment(params.dateTo).diff(moment(params.dateFrom), 'months')) {
+          case 1:
+            params.dateSpanType = CardDateSpan.Month;
+            break;
+          case 3:
+            params.dateSpanType = CardDateSpan.ThreeMonths;
+            break;
+          case 5:
+            params.dateSpanType = CardDateSpan.FiveMonths;
+            break;
+          default:
+            throw new Error("No date span found");
+        }
+      }
+
+    }
+
+    Object.assign(this, params)
+  }
+
+  dateSpanType?: CardDateSpan
+  dateFrom: Date = moment(new Date()).subtract(1, 'month').toDate();
+  dateTo: Date = new Date();
+  exerciseType: ExerciseType;
+
+}
+
 @Component({
   selector: 'app-volume-card',
   templateUrl: './volume-card.component.html',
@@ -40,10 +97,19 @@ import { GetVolumeCardChartConfig } from './volume-card-chart.config';
 })
 export class VolumeCardComponent implements OnInit, OnDestroy {
 
+  // relevant things for dashboard card
+  // which needs to remember user input
+  // and also use fixed date spans
   @Input() cardId: string;
   @Input() jsonParams: string;
-  @Input() exerciseTypes: ExerciseType[];
-  params: { dateFrom: Date, dateTo: Date, exerciseType: ExerciseType }
+  @Input() useFixedDateSpans = true;
+  @Input() persistParams = true;
+
+  dateSpans = Object.values(dateSpansDict);
+  exerciseTypes: ExerciseType[];
+
+  params: CardParameters;
+  @Output() saveParamsEvent = new EventEmitter<string>();
 
   // template relevant things
   form: FormGroup;
@@ -54,7 +120,7 @@ export class VolumeCardComponent implements OnInit, OnDestroy {
 
   config: ChartConfiguration[]; // config is main driver of chart
 
-  // params
+  // relevant for chart configuration
   isMobile: boolean;
   _theme: Theme;
   _userId: string;
@@ -77,13 +143,16 @@ export class VolumeCardComponent implements OnInit, OnDestroy {
   ) { }
 
   ngOnInit() {
-
     // get props
     this.store.select(currentUserId).pipe(take(1)).subscribe(id => this._userId = id);
     this.store.select(trackEditMode).pipe(take(1)).subscribe(editMode => this._trackEditMode = editMode);
 
     // parse saved json params from user specific dashboard
-    this.initParams();
+    if (this.persistParams) {
+      this.params = new CardParameters(this.jsonParams, this.useFixedDateSpans);
+    } else {
+      this.params = new CardParameters(null, this.useFixedDateSpans);
+    }
 
     // get initializer which will be kicked off and will fetch data once everything is set up
     this._uiChange$ = this.onUIChange();
@@ -91,38 +160,26 @@ export class VolumeCardComponent implements OnInit, OnDestroy {
     // listen to changes from initializer abd prepare data accordingly
     this._subs.add(
       this._uiChange$.subscribe(val => this.prepareData(val)),
-      // this.dashboardService.saveTrackItemParams.subscribe(_ => this.saveParams())
     )
 
     // bootstrap component and kickstart everything
-    this.onInit();
+    this.init();
   }
 
-  ngOnDestroy(): void {
-    if (this.cardId != Guid.EMPTY && !this._trackEditMode && this.cardBootstrapped && this.form.valid)
+  ngOnDestroy() {
+
+    if (this.persistParams && this.cardBootstrapped && this.form.valid)
       this.saveParams();
 
     this._subs.unsubscribe();
+
   }
 
-  /**
-   * Construct params from stringified json params.. 
-   * or init new if component is new
-   */
-  initParams() {
-    this.params = JSON.parse(this.jsonParams) ?? {
-      dateFrom: new Date(),
-      dateTo: new Date(),
-      exerciseType: null
-    };
-  }
-
-  /**
-   * Initial entry point of component
+  /** Initial entry point of component
    * Fetch data for it to operate
    * Bootstrap component and kick start it with hooking on to uiChanges
    */
-  onInit() {
+  init() {
     // get first exercise types page and initialize form
     // also refresh exerciseType from jsonParams because it may be outdated (this could be expensive)
     forkJoin(
@@ -130,17 +187,18 @@ export class VolumeCardComponent implements OnInit, OnDestroy {
       this.getExerciseType(this.params?.exerciseType?.id).pipe(take(1))
     ).pipe(take(1)).subscribe(([types, type]) => {
 
-      if (types instanceof Error) return;
-      if (type instanceof Error) return;
-
-      type = type as ExerciseType;
-      this.params.exerciseType = type;
+      if (types instanceof Error || type instanceof Error) {
+        return this.error = true;
+      }
 
       if (!types || types.entities.length == 0) {
         this.error = true;
         this.details = this.translationService.instant('DASHBOARD.MISSING_EXERCISES');
         return;
       }
+
+      type = type as ExerciseType;
+      this.params.exerciseType = type;
 
       this.createForm(types.entities);
       (this._uiChange$ as ConnectableObservable<any>).connect() // now we can fetch card data
@@ -150,8 +208,7 @@ export class VolumeCardComponent implements OnInit, OnDestroy {
     );
   }
 
-  /**
-   * UI Change for things char configuration uses
+  /** UI Change for things char configuration uses
    * Theme colors 
    * unit system values (fetch new data for this)
    * User id (who's data)
@@ -166,7 +223,7 @@ export class VolumeCardComponent implements OnInit, OnDestroy {
       this.store.select(currentUserId),
       this.store.select(isMobile)
     ).pipe(
-      // distinctUntilChanged((a, b) => a[1] == b[1]), // we fetch new data only for updated unit system
+      distinctUntilChanged((a, b) => a[1] == b[1]), // we fetch new data only for updated unit system
       shareReplay(),
       publish(),
     );
@@ -189,12 +246,12 @@ export class VolumeCardComponent implements OnInit, OnDestroy {
     // if (this._metricsData)
     //   return this.getChartConfigs(this._metricsData);
 
-    this.loadCardData(this._userId, this.exerciseType.value.id, this.dateFrom.value, this.dateTo.value);
+    this.loadCardData(this._userId, this.exerciseType.value.id, this.dateFromVal, this.dateToVal);
   }
 
   /** Fetches metric data from server */
   loadCardData(userId: string, exerciseTypeId: string, dateFrom: Date, dateTo: Date) {
-    this.reportService.getDashboardVolumeReport(userId, exerciseTypeId, dateFrom, dateTo)
+    this.reportService.getDashboardVolumeReport(userId, exerciseTypeId, dateFrom, dateTo).pipe(take(1))
       .subscribe(
         (data: ChartData<number, Date>) => {
           this._metricsData = data;
@@ -221,16 +278,32 @@ export class VolumeCardComponent implements OnInit, OnDestroy {
     return this.exerciseTypeService.getOne(typeId);
   }
 
+  get exerciseType(): AbstractControl { return this.form.get('exerciseType'); }
   get dateFrom(): AbstractControl { return this.form.get('dateFrom'); }
   get dateTo(): AbstractControl { return this.form.get('dateTo'); }
-  get exerciseType(): AbstractControl { return this.form.get('exerciseType'); }
+  get dateSpan(): AbstractControl { return this.form.get('dateSpan'); }
+
+  get exerciseTypeVal(): ExerciseType { return this.exerciseType.value; }
+  get dateFromVal(): Date { return this.useFixedDateSpans ? this.dateSpan.value?.dateFrom as Date : this.dateFrom.value as Date; }
+  get dateToVal(): Date { return this.useFixedDateSpans ? new Date() : this.dateTo.value as Date; }
 
   createForm(types: ExerciseType[]) {
+
     this.form = new FormGroup({
-      exerciseType: new FormControl(this.params?.exerciseType ?? types[0], Validators.required),
-      dateFrom: new FormControl(this.params?.dateFrom ? new Date(this.params?.dateFrom) : moment(new Date()).subtract(1, 'month').toDate(), Validators.required),
-      dateTo: new FormControl(this.params?.dateTo ? new Date(this.params?.dateTo) : new Date(), Validators.required)
+      exerciseType: new FormControl(this.params.exerciseType ?? types[0], Validators.required),
     });
+
+    // if we use fixed date intervals we only have one dropdown to choose from options
+    // like on dashboard
+    if (this.useFixedDateSpans) {
+      this.form.addControl('dateSpan', new FormControl(dateSpansDict[this.params.dateSpanType] ?? dateSpansDict[0], Validators.required))
+    }
+    // else we use any date interval so user can choose which one he wants to monitor
+    // like on reports..
+    else {
+      this.form.addControl('dateFrom', new FormControl(new Date(this.params.dateFrom), Validators.required))
+      this.form.addControl('dateTo', new FormControl(new Date(this.params.dateTo), Validators.required))
+    }
 
     this.form.updateValueAndValidity();
     this.initListeners();
@@ -244,14 +317,21 @@ export class VolumeCardComponent implements OnInit, OnDestroy {
    */
   initListeners() {
     this._subs.add(
+
       // changes on inputs to fetch chart data if inputs change
-      this.dateFrom.valueChanges.subscribe(dateFrom => this.loadCardData(this._userId, this.exerciseType.value.id, dateFrom, this.dateTo.value)),
-      this.dateTo.valueChanges.subscribe(dateTo => this.loadCardData(this._userId, this.exerciseType.value.id, this.dateFrom.value, dateTo)),
       this.exerciseType.valueChanges
         .pipe(filter(val => !!val.id), map(type => type.id))
-        .subscribe(typeId => {
-          this.loadCardData(this._userId, typeId, this.dateFrom.value, this.dateTo.value)
-        }),
+        .subscribe(typeId => this.loadCardData(this._userId, typeId, this.dateFromVal, this.dateToVal)),
+
+      !this.useFixedDateSpans && this.dateFrom.valueChanges
+        .subscribe(dateFrom => this.loadCardData(this._userId, this.exerciseTypeVal.id, dateFrom, this.dateTo.value)),
+
+      !this.useFixedDateSpans && this.dateTo.valueChanges
+        .subscribe(dateTo => this.loadCardData(this._userId, this.exerciseTypeVal.id, this.dateFrom.value, dateTo)),
+
+      this.useFixedDateSpans && this.dateSpan.valueChanges.pipe(map(span => span.dateFrom))
+        .subscribe(dateFrom => this.loadCardData(this._userId, this.exerciseTypeVal.id, dateFrom, new Date())),
+
       // autocomplete input changes.. for paging of dropdown
       this.exerciseType.valueChanges
         .pipe(
@@ -275,9 +355,14 @@ export class VolumeCardComponent implements OnInit, OnDestroy {
    * Validates form and retrieves it's data if it's valid
    */
   validateAndGetFormData() {
+
     if (!this.form.valid) return null;
 
-    return (this.exerciseType.value.id, this.dateFrom.value, this.dateTo.value)
+    const exerciseTypeId = this.exerciseTypeVal;
+    const dateFrom = this.dateFromVal;
+    const dateTo = this.dateToVal;
+
+    return { exerciseTypeId, dateFrom, dateTo };
   }
 
   // display for exercise type input
@@ -288,15 +373,20 @@ export class VolumeCardComponent implements OnInit, OnDestroy {
    */
   saveParams() {
     let params = {
-      dateFrom: this.dateFrom.value,
-      dateTo: this.dateTo.value,
+      dateFrom: this.dateFromVal,
+      dateTo: this.dateToVal,
       exerciseType: { id: this.exerciseType.value.id }
     };
-
     let jsonParams = JSON.stringify(params);
-    // this.store.dispatch(updateTrackItemParams({trackItemId: this.cardId, jsonParams }));
-    this.dashboardService.updateTrackItem(this.cardId, jsonParams)
-      .pipe(take(1))
-      .subscribe((trackItem: TrackItem) => this.store.dispatch(trackItemUpdated({ trackItem })))
+
+    // TODO outsource this out..
+    // conditions for cardId and trackEditMode will be outside..
+    if (this.cardId != Guid.EMPTY && !this._trackEditMode) {
+      this.saveParamsEvent.emit(jsonParams);
+
+      this.dashboardService.updateTrackItem(this.cardId, jsonParams)
+        .pipe(take(1))
+        .subscribe((trackItem: TrackItem) => this.store.dispatch(trackItemUpdated({ trackItem })))
+    }
   }
 }
