fc053711a076206c6a9223ef48ffe876f3e35072
Marko Urh <urh.marko@gmail.com>
2020-07-05 17:05:43 +0200

    [LIFT-54] Avatar refresh fix

--- Backend/Core/Libraries/Backend.Library.AmazonS3/S3Service.cs
+++ Backend/Core/Libraries/Backend.Library.AmazonS3/S3Service.cs
@@ -76,7 +76,7 @@ namespace Backend.Library.AmazonS3
             if (string.IsNullOrWhiteSpace(url))
                 return Task.FromResult(false);
 
-            return Task.FromResult(url.Contains(_s3Settings.UrlBase) || Guid.TryParse(url, out _));
+            return Task.FromResult(url.Contains(_s3Settings.UrlBase) || url.Contains("media/"));
         }
 
         public async Task<string> GetPresignedUrlAsync(string key)
