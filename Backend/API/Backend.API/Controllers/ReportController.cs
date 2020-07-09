6129eb631758a3b6d6171714ba3e09746bfdc817
Marko Urh <urh.marko@gmail.com>
2020-07-09 22:22:56 +0200

    [LIFT-61] Report endpoints

--- Backend/API/Backend.API/Controllers/ReportController.cs
+++ Backend/API/Backend.API/Controllers/ReportController.cs
@@ -1,5 +1,7 @@
-﻿using Backend.Business.Reports.ReportsRequests.Dashboard.GetMaxReport;
+﻿using Backend.Business.Reports.ReportsRequests.Dashboard.GetBaselineVolumeWithRelativeIntensityReport;
+using Backend.Business.Reports.ReportsRequests.Dashboard.GetMaxReport;
 using Backend.Business.Reports.ReportsRequests.Dashboard.GetVolumeReport;
+using Backend.Business.Reports.ReportsRequests.Dashboard.GetVolumeWithAvgIntensityReport;
 using Backend.Business.Reports.ReportsRequests.GetBodyweightReport;
 using Backend.Business.Reports.ReportsRequests.GetTrainingReports;
 using Microsoft.AspNetCore.Mvc;
@@ -66,6 +68,34 @@ namespace Backend.API.Controllers
             ));
 
             //return Ok(await Mediator.Send(new GetMaxReportRequest(userId, exerciseTypeId, dateFrom, dateTo), cancellationToken));
+        }
+
+        [HttpGet("{userId}/{exerciseTypeId}/{dateFrom}/{dateTo}")]
+        public async Task<IActionResult> GetBaselineVolumeWithRelativeIntensityReport(Guid userId, Guid exerciseTypeId, DateTime dateFrom, DateTime dateTo, CancellationToken cancellationToken = default)
+        {
+            var key = $"Report/GetBaselineVolumeWithRelativeIntensityReport{userId}{exerciseTypeId}{dateFrom}{dateTo}";
+            AddCacheKey(key);
+
+            return Ok(await Cache.GetOrAddAsync(
+                key,
+                entry => Mediator.Send(new GetBaselineVolumeWithRelativeIntensityReportRequest(userId, exerciseTypeId, dateFrom, dateTo), cancellationToken)
+            ));
+
+            //return Ok(await Mediator.Send(new GetVolumeReportRequest(userId, exerciseTypeId, dateFrom, dateTo), cancellationToken));
+        }
+
+        [HttpGet("{userId}/{exerciseTypeId}/{dateFrom}/{dateTo}")]
+        public async Task<IActionResult> GetVolumeWithAvgIntensityReport(Guid userId, Guid exerciseTypeId, DateTime dateFrom, DateTime dateTo, CancellationToken cancellationToken = default)
+        {
+            var key = $"Report/GetVolumeWithAvgIntensityReport{userId}{exerciseTypeId}{dateFrom}{dateTo}";
+            AddCacheKey(key);
+
+            return Ok(await Cache.GetOrAddAsync(
+                key,
+                entry => Mediator.Send(new GetVolumeWithAvgIntensityReportRequest(userId, exerciseTypeId, dateFrom, dateTo), cancellationToken)
+            ));
+
+            //return Ok(await Mediator.Send(new GetVolumeReportRequest(userId, exerciseTypeId, dateFrom, dateTo), cancellationToken));
         }
     }
 }
